687a450f6c33d0aff7542df22d61170254e7c4cf
Victor Jolissaint <victor.jolissaint@infomaniak.com>
2026-02-04 11:06:40 +0100

    feat: build color tokens as markdown file

--- packages/tokens/scripts/scripts/build-tokens/src/build/outputs/markdown/build-markdown-tokens.ts
+++ packages/tokens/scripts/scripts/build-tokens/src/build/outputs/markdown/build-markdown-tokens.ts
@@ -1,8 +1,37 @@
-import type Color from 'colorjs.io';
+import { join } from 'node:path';
 import { writeFileSafe } from '../../../../../../../../../scripts/helpers/file/write-file-safe.ts';
 import type { Logger } from '../../../../../../../../../scripts/helpers/log/logger.ts';
-import { colorDesignTokenValueToColorJs } from '../../../../../../shared/dtcg/design-token/token/types/base/types/color/value/to/colorjs/color-design-token-value-to-color-js.ts';
 import { DesignTokensCollection } from '../../../../../../shared/dtcg/resolver/design-tokens-collection.ts';
-import type { GenericDesignTokensCollectionTokenWithType } from '../../../../../../shared/dtcg/resolver/token/design-tokens-collection-token.ts';
+import { designTokensCollectionTokenToCssVariableDeclaration } from '../../../../../../shared/dtcg/resolver/to/css/token/design-tokens-collection-token-to-css-variable-declaration.ts';
 import { isColorDesignTokensCollectionToken } from '../../../../../../shared/dtcg/resolver/token/types/base/color/is-color-design-tokens-collection-token.ts';
 
+export interface BuildMarkdownTokensOptions {
+  readonly collection: DesignTokensCollection;
+  readonly outputDirectory: string;
+  readonly logger: Logger;
+}
+
+export async function buildMarkdownTokens({
+  collection,
+  outputDirectory,
+  logger,
+}: BuildMarkdownTokensOptions) {
+  return logger.asyncTask('markdown', async (): Promise<void> => {
+    const colorTable: string[] = [
+      '| Color | Name | Description |',
+      '|-------|------|-------------|',
+    ];
+
+    for (const token of collection.tokens()) {
+      if (isColorDesignTokensCollectionToken(token)) {
+        const { value, name, description } =
+          designTokensCollectionTokenToCssVariableDeclaration(token);
+        const colorBox = `<div style="border-radius: 4px; width: 100%; height: 75px; background: ${value}; border: 1px solid #ccc;"></div>\`${value}\``;
+        colorTable.push(`| ${colorBox} | \`${name}\` | ${description ?? ''} |`);
+      }
+    }
+
+    const colorFilePath = join(outputDirectory, 'markdown', 'colors.md');
+    await writeFileSafe(colorFilePath, colorTable.join('\n'), { encoding: 'utf-8' });
+  });
+}
