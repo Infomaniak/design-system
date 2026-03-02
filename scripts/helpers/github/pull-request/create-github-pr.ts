325a76d1bcd7921adbef42148a862df8ddb62a72
Jordan Chapelle <jordan.chapelle@infomaniak.com>
2026-03-02 16:25:37 +0100

    feat: Automatise PR creation

---
+++ scripts/helpers/github/pull-request/create-github-pr.ts
@@ -0,0 +1,31 @@
+export interface CreateGithubPROptions {
+  readonly owner: string;
+  readonly repo: string;
+  readonly authToken: string;
+
+  // PR details
+  readonly title: string;
+  readonly body: string;
+  readonly head: string;
+  readonly base: string;
+}
+
+export async function createGithubPR({
+  owner,
+  repo,
+  authToken,
+  ...details
+}: CreateGithubPROptions): Promise<unknown> {
+  // TODO: Define promise return type
+  const response = await fetch(`https://api.github.com/repos/${owner}/${repo}/pulls`, {
+    method: 'POST',
+    headers: [
+      ['Accept', 'application/vnd.github+json'],
+      ['Authorization', `Bearer ${authToken}`],
+      ['X-GitHub-Api-Version', '2022-11-28'],
+    ],
+    body: JSON.stringify(details),
+  });
+
+  return response.json();
+}
