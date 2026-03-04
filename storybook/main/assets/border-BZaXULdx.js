52ebd0b586dddfa3ae4fd22a75f8032aaa335a28
lifaon74 <lifaon74@users.noreply.github.com>
2026-03-04 10:13:50 +0000

    deploy: d69fc85a17541bc82d1891d5fa803c3432b12390

---
+++ storybook/main/assets/border-BZaXULdx.js
@@ -0,0 +1,16 @@
+import"./preload-helper-DGWYlufl.js";import{_t as e,a as t}from"./iframe-eMZbk9-p.js";import{n}from"./lib-BJuTmQ7_.js";import{t as r}from"./dist-ekJMOIIh.js";var i=e(),a='<!-- Do not edit directly, this file was auto-generated. -->\n\n| Preview | Token Name | CSS Variable | Description |\n|---------|------------|--------------|-------------|\n| <div style=" background: #dcfce8; height: 16px; width: var(--esds-border-xs-width); border-radius: 2px; border: 1px solid #86efad; position: relative; " ></div> <div style=" margin-top: 4px; font-family: monospace; font-size: 12px; color: #6b7280; " > border-width.0 </div> | `border.xs.width` | `--esds-border-xs-width` |  |\n| <div style=" background: #dcfce8; height: 16px; width: var(--esds-border-sm-width); border-radius: 2px; border: 1px solid #86efad; position: relative; " ></div> <div style=" margin-top: 4px; font-family: monospace; font-size: 12px; color: #6b7280; " > border-width.1 </div> | `border.sm.width` | `--esds-border-sm-width` |  |\n| <div style=" background: #dcfce8; height: 16px; width: var(--esds-border-md-width); border-radius: 2px; border: 1px solid #86efad; position: relative; " ></div> <div style=" margin-top: 4px; font-family: monospace; font-size: 12px; color: #6b7280; " > border-width.2 </div> | `border.md.width` | `--esds-border-md-width` |  |\n| <div style=" background: #dcfce8; height: 16px; width: var(--esds-border-ld-width); border-radius: 2px; border: 1px solid #86efad; position: relative; " ></div> <div style=" margin-top: 4px; font-family: monospace; font-size: 12px; color: #6b7280; " > border-width.3 </div> | `border.ld.width` | `--esds-border-ld-width` |  |\n| <div style=" background: #dcfce8; height: 16px; width: var(--esds-border-xl-width); border-radius: 2px; border: 1px solid #86efad; position: relative; " ></div> <div style=" margin-top: 4px; font-family: monospace; font-size: 12px; color: #6b7280; " > border-width.4 </div> | `border.xl.width` | `--esds-border-xl-width` |  |';function o(e){let o={h1:`h1`,p:`p`,...n(),...e.components};return(0,i.jsxs)(i.Fragment,{children:[(0,i.jsx)(t,{title:`Design Tokens/T2/Border`}),`
+`,(0,i.jsx)(o.h1,{id:`t2-border-tokens`,children:`T2 Border Tokens`}),`
+`,(0,i.jsx)(o.p,{children:`T2 Border tokens provide semantic border style definitions for elements and components.`}),`
+`,(0,i.jsx)(r,{children:a}),`
+`,(0,i.jsx)(`style`,{children:`
+  table {
+    width: 100%;
+    border-collapse: collapse;
+  }
+  table th:first-child {
+    width: 30%;
+  }
+  table th:nth-of-type(2) {
+      width: 25%;
+  }
+`})]})}function s(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,i.jsx)(t,{...e,children:(0,i.jsx)(o,{...e})}):o(e)}export{s as default};
