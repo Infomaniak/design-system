4f0709d40d9456a9fd15605274354e1e68f00e05
lifaon74 <valentinrich@gmail.com>
2026-03-04 13:12:06 +0100

    feat: add publish script end notification

--- scripts/ci/publish/ci-publish.script.ts
+++ scripts/ci/publish/ci-publish.script.ts
@@ -4,8 +4,11 @@ import { fileURLToPath } from 'node:url';
 import { loadOptionallyEnvFile } from '../../helpers/env/env-file/load-optionally-env-file.ts';
 import { getEnvGithubCiConfig } from '../../helpers/github/github-ci-config/env/get-env-github-ci-config.ts';
 import type { GithubCiConfig } from '../../helpers/github/github-ci-config/github-ci-config.ts';
+import { postKchatWebhookMessage } from '../../helpers/kchat/api/post-kchat-webhook-message.ts';
+import { getEnvKchatWebhookId } from '../../helpers/kchat/env/get-env-kchat-webhook-id.ts';
 import { DEFAULT_LOG_LEVEL } from '../../helpers/log/log-level/defaults/default-log-level.ts';
 import { Logger } from '../../helpers/log/logger.ts';
+import { dedent } from '../../helpers/misc/string/dedent/dedent.ts';
 import { getEnvCiPublishDryRun } from '../../helpers/publish/env/get-env-ci-publish-dry-run.ts';
 import { ciPublish } from './src/ci-publish.ts';
 import {
@@ -23,6 +26,7 @@ export function ciPublishScript(): Promise<void> {
 
     const githubCiConfig: GithubCiConfig = getEnvGithubCiConfig();
     const ciPublishContext: CiPublishContext = inferCiPublishContext(githubCiConfig);
+    const dryRun: boolean = getEnvCiPublishDryRun();
 
     if (!ciPublishContext.shouldPublish) {
       logger.info(
@@ -31,12 +35,49 @@ export function ciPublishScript(): Promise<void> {
       return;
     }
 
-    await ciPublish({
-      ...ciPublishContext,
-      rootDirectory: ROOT_DIR,
-      dryRun: getEnvCiPublishDryRun(),
-      logger,
-    });
+    const jobUrl: string = `${githubCiConfig.server_url}/${githubCiConfig.repository}/actions/runs/${githubCiConfig.run_id}`;
+
+    try {
+      await ciPublish({
+        ...ciPublishContext,
+        rootDirectory: ROOT_DIR,
+        dryRun,
+        logger,
+      });
+    } catch (error: unknown) {
+      if (!dryRun) {
+        await logger.asyncTask('send-kchat-notification', async (): Promise<void> => {
+          await postKchatWebhookMessage({
+            webhookId: getEnvKchatWebhookId(),
+            text: dedent`
+              #### ❌ publish job failed
+    
+              - 🔗 ${jobUrl}
+              - 🌱 ${ciPublishContext.branchName}
+              - ⚙️ ${ciPublishContext.mode}
+              - 💬 ${Error.isError(error) ? error.message : String(error)}
+            `,
+          });
+        });
+      }
+
+      throw error;
+    }
+
+    if (!dryRun && ciPublishContext.mode !== 'dev') {
+      await logger.asyncTask('send-kchat-notification', async (): Promise<void> => {
+        await postKchatWebhookMessage({
+          webhookId: getEnvKchatWebhookId(),
+          text: dedent`
+            #### ✅ publish job succeed
+    
+              - 🔗 ${jobUrl}
+              - 🌱 ${ciPublishContext.branchName}
+              - ⚙️ ${ciPublishContext.mode}
+          `,
+        });
+      });
+    }
   });
 }
 
