b626a45f277b990cbfb6a93d57cd09b09dcc01c8
vjo <vjo@users.noreply.github.com>
2026-03-05 07:13:27 +0000

    deploy: 54205be4b34b5334386ba55e8ce08d355992fdfa

---
+++ storybook/main/assets/opacity-B7FxMJhu.js
@@ -0,0 +1,266 @@
+import"./preload-helper-DGWYlufl.js";import{_t as e,a as t}from"./iframe-CXP6AQHP.js";import{n}from"./lib-ZlB3MxCM.js";import{t as r}from"./dist-acDw-gee.js";var i=e(),a=`<!-- Do not edit directly, this file was auto-generated. -->
+
+<table class="token-table">
+  <thead>
+    <tr>
+      <th style="width: 20%;">Preview</th>
+      <th style="width: 80%;">Details</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-opacity-0); border-radius: 2px; border: 1px solid #86efad; position: relative; " ></div> <div style=" margin-top: 4px; font-family: monospace; font-size: 12px; color: #6b7280; " > 0px </div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="opacity.0" type="button">opacity.0</button>
+        </div>
+        <div class="token-row">
+          CSS: <button class="token-value" data-clipboard="var(--esds-opacity-0)" type="button">var(--esds-opacity-0)</button>
+        </div>
+        <div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-opacity-5); border-radius: 2px; border: 1px solid #86efad; position: relative; " ></div> <div style=" margin-top: 4px; font-family: monospace; font-size: 12px; color: #6b7280; " > 5px </div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="opacity.5" type="button">opacity.5</button>
+        </div>
+        <div class="token-row">
+          CSS: <button class="token-value" data-clipboard="var(--esds-opacity-5)" type="button">var(--esds-opacity-5)</button>
+        </div>
+        <div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-opacity-10); border-radius: 2px; border: 1px solid #86efad; position: relative; " ></div> <div style=" margin-top: 4px; font-family: monospace; font-size: 12px; color: #6b7280; " > 10px </div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="opacity.10" type="button">opacity.10</button>
+        </div>
+        <div class="token-row">
+          CSS: <button class="token-value" data-clipboard="var(--esds-opacity-10)" type="button">var(--esds-opacity-10)</button>
+        </div>
+        <div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-opacity-15); border-radius: 2px; border: 1px solid #86efad; position: relative; " ></div> <div style=" margin-top: 4px; font-family: monospace; font-size: 12px; color: #6b7280; " > 15px </div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="opacity.15" type="button">opacity.15</button>
+        </div>
+        <div class="token-row">
+          CSS: <button class="token-value" data-clipboard="var(--esds-opacity-15)" type="button">var(--esds-opacity-15)</button>
+        </div>
+        <div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-opacity-20); border-radius: 2px; border: 1px solid #86efad; position: relative; " ></div> <div style=" margin-top: 4px; font-family: monospace; font-size: 12px; color: #6b7280; " > 20px </div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="opacity.20" type="button">opacity.20</button>
+        </div>
+        <div class="token-row">
+          CSS: <button class="token-value" data-clipboard="var(--esds-opacity-20)" type="button">var(--esds-opacity-20)</button>
+        </div>
+        <div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-opacity-25); border-radius: 2px; border: 1px solid #86efad; position: relative; " ></div> <div style=" margin-top: 4px; font-family: monospace; font-size: 12px; color: #6b7280; " > 25px </div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="opacity.25" type="button">opacity.25</button>
+        </div>
+        <div class="token-row">
+          CSS: <button class="token-value" data-clipboard="var(--esds-opacity-25)" type="button">var(--esds-opacity-25)</button>
+        </div>
+        <div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-opacity-30); border-radius: 2px; border: 1px solid #86efad; position: relative; " ></div> <div style=" margin-top: 4px; font-family: monospace; font-size: 12px; color: #6b7280; " > 30px </div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="opacity.30" type="button">opacity.30</button>
+        </div>
+        <div class="token-row">
+          CSS: <button class="token-value" data-clipboard="var(--esds-opacity-30)" type="button">var(--esds-opacity-30)</button>
+        </div>
+        <div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-opacity-35); border-radius: 2px; border: 1px solid #86efad; position: relative; " ></div> <div style=" margin-top: 4px; font-family: monospace; font-size: 12px; color: #6b7280; " > 35px </div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="opacity.35" type="button">opacity.35</button>
+        </div>
+        <div class="token-row">
+          CSS: <button class="token-value" data-clipboard="var(--esds-opacity-35)" type="button">var(--esds-opacity-35)</button>
+        </div>
+        <div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-opacity-40); border-radius: 2px; border: 1px solid #86efad; position: relative; " ></div> <div style=" margin-top: 4px; font-family: monospace; font-size: 12px; color: #6b7280; " > 40px </div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="opacity.40" type="button">opacity.40</button>
+        </div>
+        <div class="token-row">
+          CSS: <button class="token-value" data-clipboard="var(--esds-opacity-40)" type="button">var(--esds-opacity-40)</button>
+        </div>
+        <div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-opacity-45); border-radius: 2px; border: 1px solid #86efad; position: relative; " ></div> <div style=" margin-top: 4px; font-family: monospace; font-size: 12px; color: #6b7280; " > 45px </div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="opacity.45" type="button">opacity.45</button>
+        </div>
+        <div class="token-row">
+          CSS: <button class="token-value" data-clipboard="var(--esds-opacity-45)" type="button">var(--esds-opacity-45)</button>
+        </div>
+        <div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-opacity-50); border-radius: 2px; border: 1px solid #86efad; position: relative; " ></div> <div style=" margin-top: 4px; font-family: monospace; font-size: 12px; color: #6b7280; " > 50px </div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="opacity.50" type="button">opacity.50</button>
+        </div>
+        <div class="token-row">
+          CSS: <button class="token-value" data-clipboard="var(--esds-opacity-50)" type="button">var(--esds-opacity-50)</button>
+        </div>
+        <div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-opacity-55); border-radius: 2px; border: 1px solid #86efad; position: relative; " ></div> <div style=" margin-top: 4px; font-family: monospace; font-size: 12px; color: #6b7280; " > 55px </div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="opacity.55" type="button">opacity.55</button>
+        </div>
+        <div class="token-row">
+          CSS: <button class="token-value" data-clipboard="var(--esds-opacity-55)" type="button">var(--esds-opacity-55)</button>
+        </div>
+        <div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-opacity-60); border-radius: 2px; border: 1px solid #86efad; position: relative; " ></div> <div style=" margin-top: 4px; font-family: monospace; font-size: 12px; color: #6b7280; " > 60px </div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="opacity.60" type="button">opacity.60</button>
+        </div>
+        <div class="token-row">
+          CSS: <button class="token-value" data-clipboard="var(--esds-opacity-60)" type="button">var(--esds-opacity-60)</button>
+        </div>
+        <div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-opacity-65); border-radius: 2px; border: 1px solid #86efad; position: relative; " ></div> <div style=" margin-top: 4px; font-family: monospace; font-size: 12px; color: #6b7280; " > 65px </div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="opacity.65" type="button">opacity.65</button>
+        </div>
+        <div class="token-row">
+          CSS: <button class="token-value" data-clipboard="var(--esds-opacity-65)" type="button">var(--esds-opacity-65)</button>
+        </div>
+        <div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-opacity-70); border-radius: 2px; border: 1px solid #86efad; position: relative; " ></div> <div style=" margin-top: 4px; font-family: monospace; font-size: 12px; color: #6b7280; " > 70px </div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="opacity.70" type="button">opacity.70</button>
+        </div>
+        <div class="token-row">
+          CSS: <button class="token-value" data-clipboard="var(--esds-opacity-70)" type="button">var(--esds-opacity-70)</button>
+        </div>
+        <div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-opacity-75); border-radius: 2px; border: 1px solid #86efad; position: relative; " ></div> <div style=" margin-top: 4px; font-family: monospace; font-size: 12px; color: #6b7280; " > 75px </div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="opacity.75" type="button">opacity.75</button>
+        </div>
+        <div class="token-row">
+          CSS: <button class="token-value" data-clipboard="var(--esds-opacity-75)" type="button">var(--esds-opacity-75)</button>
+        </div>
+        <div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-opacity-80); border-radius: 2px; border: 1px solid #86efad; position: relative; " ></div> <div style=" margin-top: 4px; font-family: monospace; font-size: 12px; color: #6b7280; " > 80px </div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="opacity.80" type="button">opacity.80</button>
+        </div>
+        <div class="token-row">
+          CSS: <button class="token-value" data-clipboard="var(--esds-opacity-80)" type="button">var(--esds-opacity-80)</button>
+        </div>
+        <div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-opacity-85); border-radius: 2px; border: 1px solid #86efad; position: relative; " ></div> <div style=" margin-top: 4px; font-family: monospace; font-size: 12px; color: #6b7280; " > 85px </div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="opacity.85" type="button">opacity.85</button>
+        </div>
+        <div class="token-row">
+          CSS: <button class="token-value" data-clipboard="var(--esds-opacity-85)" type="button">var(--esds-opacity-85)</button>
+        </div>
+        <div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-opacity-90); border-radius: 2px; border: 1px solid #86efad; position: relative; " ></div> <div style=" margin-top: 4px; font-family: monospace; font-size: 12px; color: #6b7280; " > 90px </div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="opacity.90" type="button">opacity.90</button>
+        </div>
+        <div class="token-row">
+          CSS: <button class="token-value" data-clipboard="var(--esds-opacity-90)" type="button">var(--esds-opacity-90)</button>
+        </div>
+        <div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-opacity-95); border-radius: 2px; border: 1px solid #86efad; position: relative; " ></div> <div style=" margin-top: 4px; font-family: monospace; font-size: 12px; color: #6b7280; " > 95px </div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="opacity.95" type="button">opacity.95</button>
+        </div>
+        <div class="token-row">
+          CSS: <button class="token-value" data-clipboard="var(--esds-opacity-95)" type="button">var(--esds-opacity-95)</button>
+        </div>
+        <div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-opacity-100); border-radius: 2px; border: 1px solid #86efad; position: relative; " ></div> <div style=" margin-top: 4px; font-family: monospace; font-size: 12px; color: #6b7280; " > 100px </div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="opacity.100" type="button">opacity.100</button>
+        </div>
+        <div class="token-row">
+          CSS: <button class="token-value" data-clipboard="var(--esds-opacity-100)" type="button">var(--esds-opacity-100)</button>
+        </div>
+        <div class="token-row token-description"></div>
+      </td>
+    </tr>
+  </tbody>
+</table>`;function o(e){let o={h1:`h1`,...n(),...e.components};return(0,i.jsxs)(i.Fragment,{children:[(0,i.jsx)(t,{title:`Design Tokens/T1/Opacity`}),`
+`,(0,i.jsx)(o.h1,{id:`t1-opacity-tokens`,children:`T1 Opacity Tokens`}),`
+`,(0,i.jsx)(r,{children:a})]})}function s(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,i.jsx)(t,{...e,children:(0,i.jsx)(o,{...e})}):o(e)}export{s as default};
