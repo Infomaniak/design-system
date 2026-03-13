fee766ac789a406f98192f86ff061671f29e6c85
lifaon74 <valentinrich@gmail.com>
2026-03-13 16:37:29 +0100

    chore: remove comment

--- packages/assets/images/svg/scripts/scripts/import-svgs/src/import/icons/import-and-build-figma-icons.ts
+++ packages/assets/images/svg/scripts/scripts/import-svgs/src/import/icons/import-and-build-figma-icons.ts
@@ -4,11 +4,6 @@ import {
   type ExtractSvgFilesFromFigmaDesignFileAndBuildSetOptions,
 } from '../../../../../shared/svg/figma/extract-svg-files-from-figma-design-file-and-build-set.ts';
 
-/*
-
-  The figma "icons" design: https://www.figma.com/design/15F6clEEytVf34fPCGoRfs/IKDS---Icons?node-id=0-1&p=f&t=XZLqlGGsohWqWGrH-0
-*/
-
 export interface ImportAndBuildFigmaIconsOptions extends Omit<
   ExtractSvgFilesFromFigmaDesignFileAndBuildSetOptions,
   'prefix' | 'svgImagesOutputDirectory' | 'svgSetOutputDirectory' | 'hasStockedVersion' | 'monotone'
