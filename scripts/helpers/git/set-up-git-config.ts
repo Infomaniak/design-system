b645da92c04ad7f7dfbc7307d20bb784f0789541
lifaon74 <valentinrich@gmail.com>
2026-03-19 12:26:15 +0100

    chore: fix typo

--- scripts/helpers/git/set-up-git-config.ts
+++ scripts/helpers/git/set-up-git-config.ts
@@ -15,8 +15,8 @@ export async function setUpGitConfig({
   userName = 'github-actions',
   userEmail = 'github-actions@github.com',
   logger,
-  ...spawOptions
+  ...spawnOptions
 }: SetUpGitGlobalConfigOptions): Promise<void> {
-  await execCommandInherit(logger, 'git', ['config', 'user.name', userName], spawOptions);
-  await execCommandInherit(logger, 'git', ['config', 'user.email', userEmail], spawOptions);
+  await execCommandInherit(logger, 'git', ['config', 'user.name', userName], spawnOptions);
+  await execCommandInherit(logger, 'git', ['config', 'user.email', userEmail], spawnOptions);
 }
