8782e631170285bc5c08f37fb8e6b85e335da950
vjo <vjo@users.noreply.github.com>
2026-03-31 13:21:50 +0000

    deploy: 8ae595e836ffb5723ca78a3911dd04b470cc22d5

---
+++ storybook/mr/87/assets/react-DWJ2E_Is.js
@@ -0,0 +1,1 @@
+import{n as e,t}from"./lib-BrI7F4mq.js";export{t as MDXProvider};
---
+++ storybook/mr/87/assets/t3-DE2z6wab.js
@@ -0,0 +1,19 @@
+import{Yt as e,_t as t,a as n}from"./iframe-BW7zWjlV.js";import{n as r}from"./lib-BrI7F4mq.js";var i=e(t());function a(e){let t={a:`a`,code:`code`,h1:`h1`,h2:`h2`,h3:`h3`,li:`li`,p:`p`,strong:`strong`,ul:`ul`,...r(),...e.components},{Table:a}=t;return a||s(`Table`,!0),(0,i.jsxs)(i.Fragment,{children:[(0,i.jsx)(n,{title:`Design Tokens/T3`}),`
+`,(0,i.jsx)(t.h1,{id:`t3-component-tokens`,children:`T3 Component Tokens`}),`
+`,(0,i.jsx)(t.h2,{id:`what-are-t3-tokens`,children:`What are T3 Tokens?`}),`
+`,(0,i.jsx)(t.p,{children:`T3 tokens are component-specific design tokens that reference T1 (primitive) and T2 (semantic) values. They define visual properties for specific components.`}),`
+`,(0,i.jsx)(t.h3,{id:`characteristics`,children:`Characteristics:`}),`
+`,(0,i.jsxs)(t.ul,{children:[`
+`,(0,i.jsxs)(t.li,{children:[(0,i.jsx)(t.strong,{children:`Component-specific`}),`: Named after a specific component`]}),`
+`,(0,i.jsxs)(t.li,{children:[(0,i.jsx)(t.strong,{children:`Reference T1 and T2`}),`: Points to lower tier tokens`]}),`
+`,(0,i.jsxs)(t.li,{children:[(0,i.jsx)(t.strong,{children:`Modifiers`}),`: Modifiers could override T3 tokens to adapt to a specific use cases`]}),`
+`,(0,i.jsxs)(t.li,{children:[(0,i.jsx)(t.strong,{children:`Single component usage`}),`: Used by one specific component`]}),`
+`,(0,i.jsxs)(t.li,{children:[(0,i.jsx)(t.strong,{children:`Different variants`}),`: Supports component states (hover, active, disabled)`]}),`
+`]}),`
+`,(0,i.jsx)(t.h3,{id:`examples`,children:`Examples:`}),`
+`,(0,i.jsxs)(t.ul,{children:[`
+`,(0,i.jsxs)(t.li,{children:[(0,i.jsx)(t.code,{children:`button.background.hover`}),` using `,(0,i.jsx)(t.code,{children:`color.background.primary.hover`})]}),`
+`,(0,i.jsxs)(t.li,{children:[(0,i.jsx)(t.code,{children:`card.padding`}),` using `,(0,i.jsx)(t.code,{children:`spacing.section.medium`})]}),`
+`]}),`
+`,(0,i.jsx)(t.h3,{id:`token-categories-in-t3`,children:`Token Categories in T3:`}),`
+`,(0,i.jsx)(a,{headers:[`Category`,`Description`],rows:[[(0,i.jsx)(t.a,{href:`/docs/design-tokens-t3-typography--docs`,children:`Typography`}),`Typography component styles`]]})]})}function o(e={}){let{wrapper:t}={...r(),...e.components};return t?(0,i.jsx)(t,{...e,children:(0,i.jsx)(a,{...e})}):a(e)}function s(e,t){throw Error(`Expected `+(t?`component`:`object`)+" `"+e+"` to be defined: you likely forgot to import, pass, or provide it.")}export{o as default};
