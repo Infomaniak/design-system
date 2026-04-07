c817c1907a58310bfcbc966732d8a81d4c35b4ac
Victor Jolissaint <victor.jolissaint@infomaniak.com>
2026-04-07 10:30:16 +0200

    tests: use happy-dom

--- apps/docs/src/components/IconGallery.test.tsx
+++ apps/docs/src/components/IconGallery.test.tsx
@@ -1,5 +1,5 @@
 /**
- * @vitest-environment jsdom
+ * @vitest-environment happy-dom
  */
 
 import { cleanup, render } from '@testing-library/react';
--- apps/docs/src/hooks/useIconGallery.test.ts
+++ apps/docs/src/hooks/useIconGallery.test.ts
@@ -1,5 +1,5 @@
 /**
- * @vitest-environment jsdom
+ * @vitest-environment happy-dom
  */
 
 import { act, renderHook, waitFor } from '@testing-library/react';
