6ad038dec4b104bbfa41b5394937aa436f3d2e52
lifaon74 <valentinrich@gmail.com>
2026-04-09 09:09:53 +0200

    feat(tokens): improve figma<->code bridge => sort modes

--- packages/tokens/scripts/shared/tokens-bruecke/tokens-bruecke/group/types/typography/typography-tokens-bruecke-design-tokens-group.ts
+++ packages/tokens/scripts/shared/tokens-bruecke/tokens-bruecke/group/types/typography/typography-tokens-bruecke-design-tokens-group.ts
@@ -3,6 +3,7 @@ import type { StringTokensBrueckeDesignToken } from '../../../token/types/string
 import type { TokensBrueckeDesignTokensGroup } from '../../tokens-bruecke-design-tokens-group.ts';
 
 export interface TypographyTokensBrueckeDesignTokensGroup extends TokensBrueckeDesignTokensGroup {
+  // TODO move to DTCG when figma is ready
   readonly family: StringTokensBrueckeDesignToken;
   readonly weight: StringTokensBrueckeDesignToken;
   readonly size: DimensionTokensBrueckeDesignToken;
