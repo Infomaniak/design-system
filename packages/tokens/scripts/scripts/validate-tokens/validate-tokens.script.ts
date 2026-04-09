df91c8774e10bdae58b005d30b091f942a57b1c2
lifaon74 <valentinrich@gmail.com>
2026-04-09 08:45:42 +0200

    chore(tokens): improve schema validation

--- packages/tokens/scripts/scripts/validate-tokens/validate-tokens.script.ts
+++ packages/tokens/scripts/scripts/validate-tokens/validate-tokens.script.ts
@@ -1,13 +1,13 @@
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { validateDesignTokenFiles } from '../../shared/dtcg/operations/validate/files/validate-design-token-files.ts';
+import { validateDesignTokenFilesSchema } from '../../shared/dtcg/operations/validate/schema/files/validate-design-token-files-schema.ts';
 
 const LOCAL_DIR: string = dirname(fileURLToPath(import.meta.url));
 const ROOT_DIR: string = join(LOCAL_DIR, '../../..');
 const TOKENS_DIR: string = join(ROOT_DIR, 'tokens');
 
 export async function validateTokensScript(): Promise<void> {
-  await validateDesignTokenFiles([`${TOKENS_DIR}/**/*.tokens.json`]);
+  await validateDesignTokenFilesSchema([`${TOKENS_DIR}/**/*.tokens.json`]);
 }
 
 await validateTokensScript();
