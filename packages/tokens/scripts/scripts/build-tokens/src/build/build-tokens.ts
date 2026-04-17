398088fafa46d3e045a1bd54f47d9cdcb1158856
lifaon74 <valentinrich@gmail.com>
2026-04-17 10:51:52 +0200

    feat(tokens): add modifiers validation

--- packages/tokens/scripts/scripts/build-tokens/src/build/build-tokens.ts
+++ packages/tokens/scripts/scripts/build-tokens/src/build/build-tokens.ts
@@ -1,17 +1,25 @@
 import type { BuildConfig } from '../../../../../../../scripts/helpers/build/build-config/build-config.ts';
 import type { Logger } from '../../../../../../../scripts/helpers/log/logger.ts';
 import { removeTrailingSlash } from '../../../../../../../scripts/helpers/path/remove-traling-slash.ts';
+import { ensureDesignTokenFilesContainsOnlyReferences } from '../../../../shared/dtcg/operations/validate/data/ensure-contains-only-references/files/ensure-design-token-files-contains-only-references.ts';
+import { validateDesignTokenFilesSchema } from '../../../../shared/dtcg/operations/validate/schema/files/validate-design-token-files-schema.ts';
 import { DesignTokensCollection } from '../../../../shared/dtcg/resolver/design-tokens-collection.ts';
 import {
   type DesignTokenModifiers,
   extractDesignTokenModifiers,
 } from '../../../../shared/dtcg/resolver/modifiers/design-token-modifiers.ts';
-import { DESIGN_TOKEN_TIERS, MODIFIERS_DIRECTORY_NAME } from '../constants/design-token-tiers.ts';
+import {
+  DESIGN_TOKEN_TIERS,
+  MODIFIERS_DIRECTORY_NAME,
+  T2_DIRECTORY_NAME,
+  T3_DIRECTORY_NAME,
+} from '../constants/design-token-tiers.ts';
 import { buildCssTokens } from './outputs/css/build-css-tokens.ts';
 import { buildFigmaTokens } from './outputs/figma/build-figma-tokens.ts';
 import { buildKotlinTokens } from './outputs/kotlin/build-kotlin-tokens.ts';
 import { buildMarkdownTokens } from './outputs/markdown/build-markdown-tokens.ts';
 import { buildSwiftTokens } from './outputs/swift/build-swift-tokens.ts';
+import { validateModifiers } from './validate/validate-modifiers.ts';
 
 export interface BuildTokensOptions extends BuildConfig {
   readonly sourceDirectory: string;
@@ -28,6 +36,21 @@ export function buildTokens({
     sourceDirectory = removeTrailingSlash(sourceDirectory);
     outputDirectory = removeTrailingSlash(outputDirectory);
 
+    // VALIDATE SCHEMA
+    await logger.asyncTask('validate-schema', (): Promise<void> => {
+      return validateDesignTokenFilesSchema([`${sourceDirectory}/**/*.tokens.json`]);
+    });
+
+    // VALIDATE that T2, T3 and MODIFIERS contains only references
+    await logger.asyncTask('validate-tokens-contain-only-references', (): Promise<void> => {
+      return ensureDesignTokenFilesContainsOnlyReferences([
+        `${sourceDirectory}/${T2_DIRECTORY_NAME}/*.tokens.json`,
+        `${sourceDirectory}/${T3_DIRECTORY_NAME}/*.tokens.json`,
+        `${sourceDirectory}/${MODIFIERS_DIRECTORY_NAME}/*.tokens.json`,
+      ]);
+    });
+
+    // IMPORT T1, T2, T3, TOKENS
     const baseCollection: DesignTokensCollection = await new DesignTokensCollection().fromFiles(
       DESIGN_TOKEN_TIERS.map(
         (tier: string): string => `${sourceDirectory}/${tier}/**/*.tokens.json`,
@@ -41,6 +64,11 @@ export function buildTokens({
     const modifiers: DesignTokenModifiers = await extractDesignTokenModifiers({
       sourceDirectory: `${sourceDirectory}/${MODIFIERS_DIRECTORY_NAME}`,
       baseCollection,
+    });
+
+    // VALIDATE MODIFIERS
+    await logger.asyncTask('validate-modifiers', (): void => {
+      validateModifiers(modifiers);
     });
 
     // CSS
