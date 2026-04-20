88b8cf53e76909aad2c0742bfab8944cfca69efa
lifaon74 <valentinrich@gmail.com>
2026-04-20 08:42:09 +0200

    chore(assets-image-svg-component-web): fix docs generation

--- packages/assets/images/component/web/src/public-api.ts
+++ packages/assets/images/component/web/src/public-api.ts
@@ -9,5 +9,6 @@ export * from './api/types/api/list-icons/iconify-api-list-icons-options.ts';
 export * from './api/types/api/list-icons/iconify-api-list-icons-response.ts';
 export * from './api/types/custom/get-svg/iconify-api-get-svg-options.ts';
 export * from './api/types/custom/search/icon-list/iconify-api-icon-list.ts';
+export * from './api/types/custom/search/icon-list/iconify-api-list-icons-response-to-iconify-api-icon-list.ts';
 export * from './api/types/custom/search/iconify-api-search-options.ts';
 export * from './svg-component/esds-icon.component.ts';
