8ba4bed91239fd6efedf94ef930f36eb68fcd010
lifaon74 <lifaon74@users.noreply.github.com>
2026-04-23 06:14:50 +0000

    deploy: bb7532e4d1c8d20b1ecc0e6030791316ab627e0b

---
+++ storybook/mr/113/assets/t3-Dlxg8lFI.js
@@ -0,0 +1,19 @@
+import{a as e,en as t,s as n,yt as r}from"./iframe-Nh1rIxcu.js";var i=t(r());function a(t){let r={a:`a`,code:`code`,h1:`h1`,h2:`h2`,h3:`h3`,li:`li`,p:`p`,strong:`strong`,ul:`ul`,...n(),...t.components},{Table:a}=r;return a||s(`Table`,!0),(0,i.jsxs)(i.Fragment,{children:[(0,i.jsx)(e,{title:`Design Tokens/T3`}),`
+`,(0,i.jsx)(r.h1,{id:`t3-component-tokens`,children:`T3 Component Tokens`}),`
+`,(0,i.jsx)(r.h2,{id:`what-are-t3-tokens`,children:`What are T3 Tokens?`}),`
+`,(0,i.jsx)(r.p,{children:`T3 tokens are component-specific design tokens that reference T1 (primitive) and T2 (semantic) values. They define visual properties for specific components.`}),`
+`,(0,i.jsx)(r.h3,{id:`characteristics`,children:`Characteristics:`}),`
+`,(0,i.jsxs)(r.ul,{children:[`
+`,(0,i.jsxs)(r.li,{children:[(0,i.jsx)(r.strong,{children:`Component-specific`}),`: Named after a specific component`]}),`
+`,(0,i.jsxs)(r.li,{children:[(0,i.jsx)(r.strong,{children:`Reference T1 and T2`}),`: Points to lower tier tokens`]}),`
+`,(0,i.jsxs)(r.li,{children:[(0,i.jsx)(r.strong,{children:`Modifiers`}),`: Modifiers could override T3 tokens to adapt to a specific use cases`]}),`
+`,(0,i.jsxs)(r.li,{children:[(0,i.jsx)(r.strong,{children:`Single component usage`}),`: Used by one specific component`]}),`
+`,(0,i.jsxs)(r.li,{children:[(0,i.jsx)(r.strong,{children:`Different variants`}),`: Supports component states (hover, active, disabled)`]}),`
+`]}),`
+`,(0,i.jsx)(r.h3,{id:`examples`,children:`Examples:`}),`
+`,(0,i.jsxs)(r.ul,{children:[`
+`,(0,i.jsxs)(r.li,{children:[(0,i.jsx)(r.code,{children:`button.background.hover`}),` using `,(0,i.jsx)(r.code,{children:`color.background.primary.hover`})]}),`
+`,(0,i.jsxs)(r.li,{children:[(0,i.jsx)(r.code,{children:`card.padding`}),` using `,(0,i.jsx)(r.code,{children:`spacing.section.medium`})]}),`
+`]}),`
+`,(0,i.jsx)(r.h3,{id:`token-categories-in-t3`,children:`Token Categories in T3:`}),`
+`,(0,i.jsx)(a,{headers:[`Category`,`Description`],rows:[[(0,i.jsx)(r.a,{href:`/docs/design-tokens-t3-typography--docs`,children:`Typography`}),`Typography component styles`]]})]})}function o(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,i.jsx)(t,{...e,children:(0,i.jsx)(a,{...e})}):a(e)}function s(e,t){throw Error(`Expected `+(t?`component`:`object`)+" `"+e+"` to be defined: you likely forgot to import, pass, or provide it.")}export{o as default};
