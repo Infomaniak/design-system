d846d2f4b7364a9d9df1281c35b8fe1288f5cfcf
vjo <vjo@users.noreply.github.com>
2026-06-04 15:59:49 +0000

    deploy: 3d66b00945f90f1ae4ac0666900fe1a19150ebc0

---
+++ storybook/mr/168/assets/border-DspkI0SZ.js
@@ -0,0 +1,65 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{T as t,a as n,o as r,u as i}from"./iframe-DbLUAqCm.js";import{t as a}from"./mdx-react-shim-DHnWyy7Y.js";import{n as o,t as s}from"./dist-D_Au07Dq.js";var c,l=e((()=>{c=`<!-- Do not edit directly, this file was auto-generated. -->
+
+<table class="token-table">
+  <thead>
+    <tr>
+      <th style="width: 20%;">Preview</th>
+      <th style="width: 80%;">Details</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td><div style=" width: 50px; height: 50px; background: #f1f5f9; border: var(--esds-border-xs-width) solid #374151; display: inline-block; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="border.xs.width" type="button">border.xs.width</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-border-xs-width)" type="button">var(--esds-border-xs-width)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" width: 50px; height: 50px; background: #f1f5f9; border: var(--esds-border-sm-width) solid #374151; display: inline-block; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="border.sm.width" type="button">border.sm.width</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-border-sm-width)" type="button">var(--esds-border-sm-width)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" width: 50px; height: 50px; background: #f1f5f9; border: var(--esds-border-md-width) solid #374151; display: inline-block; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="border.md.width" type="button">border.md.width</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-border-md-width)" type="button">var(--esds-border-md-width)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" width: 50px; height: 50px; background: #f1f5f9; border: var(--esds-border-ld-width) solid #374151; display: inline-block; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="border.ld.width" type="button">border.ld.width</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-border-ld-width)" type="button">var(--esds-border-ld-width)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" width: 50px; height: 50px; background: #f1f5f9; border: var(--esds-border-xl-width) solid #374151; display: inline-block; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="border.xl.width" type="button">border.xl.width</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-border-xl-width)" type="button">var(--esds-border-xl-width)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+  </tbody>
+</table>`}));function u(e){let t={h1:`h1`,p:`p`,...i(),...e.components};return(0,f.jsxs)(f.Fragment,{children:[(0,f.jsx)(n,{title:`Design Tokens/T2/Border`}),`
+`,(0,f.jsx)(t.h1,{id:`t2-border-tokens`,children:`T2 Border Tokens`}),`
+`,(0,f.jsx)(t.p,{children:`T2 Border tokens provide semantic border style definitions for elements and components.`}),`
+`,(0,f.jsx)(s,{children:c})]})}function d(e={}){let{wrapper:t}={...i(),...e.components};return t?(0,f.jsx)(t,{...e,children:(0,f.jsx)(u,{...e})}):u(e)}var f;e((()=>{f=t(),a(),r(),l(),o()}))();export{d as default};
