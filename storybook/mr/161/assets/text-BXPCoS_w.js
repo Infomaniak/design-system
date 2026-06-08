ee2cb86dfc9c7f6dd081ebcf36fa2799fcdb7423
vjo <vjo@users.noreply.github.com>
2026-06-08 13:38:14 +0000

    deploy: 9ff52ea7a4bf68d3dbda47f927a8063d0d9ab220

---
+++ storybook/mr/161/assets/text-BXPCoS_w.js
@@ -0,0 +1,285 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{D as t,c as n,f as r,s as i}from"./iframe-BrkQnv--.js";import{t as a}from"./mdx-react-shim-B031RQ8z.js";import{n as o,t as s}from"./dist-DDvI68wh.js";var c,l=e((()=>{c=`<!-- Do not edit directly, this file was auto-generated. -->
+
+<table class="token-table">
+  <thead>
+    <tr>
+      <th style="width: 20%;">Preview</th>
+      <th style="width: 80%;">Details</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-text-xs-size); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text.xs.size" type="button">text.xs.size</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-xs-size)" type="button">var(--esds-text-xs-size)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-text-xs-line-height); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text.xs.line-height" type="button">text.xs.line-height</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-xs-line-height)" type="button">var(--esds-text-xs-line-height)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-text-xs-letter-spacing); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text.xs.letter-spacing" type="button">text.xs.letter-spacing</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-xs-letter-spacing)" type="button">var(--esds-text-xs-letter-spacing)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-text-sm-size); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text.sm.size" type="button">text.sm.size</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-sm-size)" type="button">var(--esds-text-sm-size)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-text-sm-line-height); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text.sm.line-height" type="button">text.sm.line-height</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-sm-line-height)" type="button">var(--esds-text-sm-line-height)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-text-sm-letter-spacing); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text.sm.letter-spacing" type="button">text.sm.letter-spacing</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-sm-letter-spacing)" type="button">var(--esds-text-sm-letter-spacing)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-text-md-size); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text.md.size" type="button">text.md.size</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-md-size)" type="button">var(--esds-text-md-size)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-text-md-line-height); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text.md.line-height" type="button">text.md.line-height</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-md-line-height)" type="button">var(--esds-text-md-line-height)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-text-md-letter-spacing); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text.md.letter-spacing" type="button">text.md.letter-spacing</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-md-letter-spacing)" type="button">var(--esds-text-md-letter-spacing)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-text-lg-size); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text.lg.size" type="button">text.lg.size</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-lg-size)" type="button">var(--esds-text-lg-size)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-text-lg-line-height); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text.lg.line-height" type="button">text.lg.line-height</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-lg-line-height)" type="button">var(--esds-text-lg-line-height)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-text-lg-letter-spacing); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text.lg.letter-spacing" type="button">text.lg.letter-spacing</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-lg-letter-spacing)" type="button">var(--esds-text-lg-letter-spacing)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-text-xl-size); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text.xl.size" type="button">text.xl.size</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-xl-size)" type="button">var(--esds-text-xl-size)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-text-xl-line-height); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text.xl.line-height" type="button">text.xl.line-height</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-xl-line-height)" type="button">var(--esds-text-xl-line-height)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-text-xl-letter-spacing); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text.xl.letter-spacing" type="button">text.xl.letter-spacing</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-xl-letter-spacing)" type="button">var(--esds-text-xl-letter-spacing)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-text-2xl-size); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text.2xl.size" type="button">text.2xl.size</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-2xl-size)" type="button">var(--esds-text-2xl-size)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-text-2xl-line-height); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text.2xl.line-height" type="button">text.2xl.line-height</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-2xl-line-height)" type="button">var(--esds-text-2xl-line-height)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-text-2xl-letter-spacing); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text.2xl.letter-spacing" type="button">text.2xl.letter-spacing</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-2xl-letter-spacing)" type="button">var(--esds-text-2xl-letter-spacing)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-text-3xl-size); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text.3xl.size" type="button">text.3xl.size</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-3xl-size)" type="button">var(--esds-text-3xl-size)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-text-3xl-line-height); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text.3xl.line-height" type="button">text.3xl.line-height</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-3xl-line-height)" type="button">var(--esds-text-3xl-line-height)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-text-3xl-letter-spacing); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text.3xl.letter-spacing" type="button">text.3xl.letter-spacing</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-3xl-letter-spacing)" type="button">var(--esds-text-3xl-letter-spacing)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-text-4xl-size); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text.4xl.size" type="button">text.4xl.size</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-4xl-size)" type="button">var(--esds-text-4xl-size)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-text-4xl-line-height); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text.4xl.line-height" type="button">text.4xl.line-height</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-4xl-line-height)" type="button">var(--esds-text-4xl-line-height)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-text-4xl-letter-spacing); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text.4xl.letter-spacing" type="button">text.4xl.letter-spacing</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-4xl-letter-spacing)" type="button">var(--esds-text-4xl-letter-spacing)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-text-5xl-size); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text.5xl.size" type="button">text.5xl.size</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-5xl-size)" type="button">var(--esds-text-5xl-size)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-text-5xl-line-height); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text.5xl.line-height" type="button">text.5xl.line-height</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-5xl-line-height)" type="button">var(--esds-text-5xl-line-height)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-text-5xl-letter-spacing); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text.5xl.letter-spacing" type="button">text.5xl.letter-spacing</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-5xl-letter-spacing)" type="button">var(--esds-text-5xl-letter-spacing)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+  </tbody>
+</table>`}));function u(e){let t={h1:`h1`,p:`p`,...r(),...e.components};return(0,f.jsxs)(f.Fragment,{children:[(0,f.jsx)(i,{title:`Design Tokens/T2/Text`}),`
+`,(0,f.jsx)(t.h1,{id:`t2-text-tokens`,children:`T2 Text Tokens`}),`
+`,(0,f.jsx)(t.p,{children:`T2 Text tokens provide text styling definitions for typography elements.`}),`
+`,(0,f.jsx)(s,{children:c})]})}function d(e={}){let{wrapper:t}={...r(),...e.components};return t?(0,f.jsx)(t,{...e,children:(0,f.jsx)(u,{...e})}):u(e)}var f;e((()=>{f=t(),a(),n(),l(),o()}))();export{d as default};
