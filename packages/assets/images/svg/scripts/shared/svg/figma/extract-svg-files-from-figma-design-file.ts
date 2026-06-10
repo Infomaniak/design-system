77f83fd9ea943d028e8ecb8217b252c32e687c1b
lifaon74 <valentinrich@gmail.com>
2026-06-10 07:51:01 +0200

    docs(icons): improve "optical-size" docs for "filled" icons

--- packages/assets/images/svg/scripts/shared/svg/figma/extract-svg-files-from-figma-design-file.ts
+++ packages/assets/images/svg/scripts/shared/svg/figma/extract-svg-files-from-figma-design-file.ts
@@ -31,6 +31,7 @@ export interface ExtractSvgFilesFromFigmaDesignFileOptions {
   readonly outputDirectory: string;
   readonly figmaAPIToken: string;
   readonly figmaSourceFileKey: string;
+  readonly generateMasks: boolean;
   readonly logger: Logger;
 }
 
@@ -42,6 +43,7 @@ export function extractSvgFilesFromFigmaDesignFile({
   outputDirectory,
   figmaAPIToken,
   figmaSourceFileKey,
+  generateMasks,
   logger,
 }: ExtractSvgFilesFromFigmaDesignFileOptions): Promise<void> {
   return logger.asyncTask('extract-figma-svgs', async (logger: Logger): Promise<void> => {
@@ -138,114 +140,125 @@ export function extractSvgFilesFromFigmaDesignFile({
       });
     });
 
-    /*
+    if (generateMasks) {
+      /*
       NOTE:
         filled component are extracted as "outlined" svgs from the previous operation.
         We'll generate true "mask" svgs from theses "filled" svgs.
      */
-    await logger.asyncTask('extract-filled-svgs', async (logger: Logger): Promise<void> => {
-      interface SVGToLoad extends SVGInnerShape {
-        readonly name: string;
-        readonly base: SVGInnerShape;
-        readonly cutouts: readonly SVGInnerShape[];
-      }
-
-      // extract the "filled" svgs from the list of component nodes present in the figma file
-      const svgsToLoad: readonly SVGToLoad[] = Array.from(
-        FigmaNodesExplorer.explore<FigmaComponentNode>(
-          figmaFile.document,
-          (node: GenericFigmaNode): TreeExplorerPickReturn | void => {
-            if (
-              isFigmaComponentNode(node) &&
-              isIconFigmaComponent(node.name) &&
-              node.children.length === 1 &&
-              isFigmaBooleanOperationNode(node.children[0]) &&
-              node.children[0].booleanOperation === 'SUBTRACT'
-            ) {
-              return {
-                pickSelf: true,
-                pickChildren: false,
-              };
-            } else {
-              return {
-                pickSelf: false,
-                pickChildren: true,
-              };
-            }
-          },
-        ),
-      ).map((node: FigmaComponentNode): SVGToLoad => {
-        const name: string = extractIconName(node.name);
-
-        if (!name.endsWith('-filled')) {
-          throw new Error(
-            `Found an icon ${JSON.stringify(name)} with SUBTRACT operation not ending with "-filled".`,
-          );
+      await logger.asyncTask('extract-filled-svgs', async (logger: Logger): Promise<void> => {
+        interface SVGToLoad extends SVGInnerShape {
+          readonly name: string;
+          readonly base: SVGInnerShape;
+          readonly cutouts: readonly SVGInnerShape[];
         }
 
-        const booleanOperation: FigmaBooleanOperationNode = node
-          .children[0] as FigmaBooleanOperationNode;
+        // extract the "filled" svgs from the list of component nodes present in the figma file
+        const svgsToLoad: readonly SVGToLoad[] = Array.from(
+          FigmaNodesExplorer.explore<FigmaComponentNode>(
+            figmaFile.document,
+            (node: GenericFigmaNode): TreeExplorerPickReturn | void => {
+              if (
+                // "filled" svgs are figma components with a "SUBTRACT" boolean operation
+                isFigmaComponentNode(node) &&
+                isIconFigmaComponent(node.name) &&
+                node.children.length === 1 &&
+                isFigmaBooleanOperationNode(node.children[0]) &&
+                node.children[0].booleanOperation === 'SUBTRACT'
+              ) {
+                return {
+                  pickSelf: true,
+                  pickChildren: false,
+                };
+              } else {
+                return {
+                  pickSelf: false,
+                  pickChildren: true,
+                };
+              }
+            },
+          ),
+        ).map((node: FigmaComponentNode): SVGToLoad => {
+          const name: string = extractIconName(node.name);
 
-        const [base, ...cutouts] = booleanOperation.children as readonly (GenericFigmaNode &
-          HavingFigmaAbsoluteBoundingBox)[];
+          if (!name.endsWith('-filled')) {
+            throw new Error(
+              `Found an icon ${JSON.stringify(name)} with SUBTRACT operation not ending with "-filled".`,
+            );
+          }
 
-        return {
-          ...figmaNodeToSVGInnerShape(node),
-          name,
-          base: figmaNodeToSVGInnerShape(base),
-          cutouts: cutouts.map(figmaNodeToSVGInnerShape),
-        };
-      });
+          const booleanOperation: FigmaBooleanOperationNode = node
+            .children[0] as FigmaBooleanOperationNode;
 
-      // get all the svgs's urls in one fetch using the figma api
-      const images: FigmaImagesRecord = await logger.asyncTask(
-        'get-urls',
-        async (): Promise<FigmaImagesRecord> => {
-          return getFigmaImages({
-            token: figmaAPIToken,
-            file_key: figmaSourceFileKey,
-            ids: svgsToLoad.flatMap(({ base, cutouts }: SVGToLoad): readonly string[] => {
-              return [base.id, ...cutouts.map((cutout: SVGInnerShape): string => cutout.id)];
-            }),
-          });
-        },
-      );
+          // a "SUBTRACT" operation contains a "base" and many "cutouts" (what is subtracted)
+          const [base, ...cutouts] = booleanOperation.children as readonly (GenericFigmaNode &
+            HavingFigmaAbsoluteBoundingBox)[];
 
-      await logger.asyncTask('store-svgs', async (logger: Logger): Promise<void> => {
-        await Promise.all(
-          svgsToLoad.map(
-            async ({ x, y, width, height, name, base, cutouts }: SVGToLoad): Promise<void> => {
-              const [baseSvg, ...cutoutSvgs] = await Promise.all([
-                fetchFigmaSvgAsset(images[base.id]),
-                ...cutouts.map((cutout: SVGInnerShape): Promise<string> => {
-                  return fetchFigmaSvgAsset(images[cutout.id]);
-                }),
-              ]);
+          return {
+            ...figmaNodeToSVGInnerShape(node),
+            name,
+            base: figmaNodeToSVGInnerShape(base),
+            cutouts: cutouts.map(figmaNodeToSVGInnerShape),
+          };
+        });
 
-              const maskId: string = `mask_${name.replace(/\W/g, '_')}`;
+        // get all the svgs's urls in one fetch using the figma api -> for the base and the cutouts
+        const images: FigmaImagesRecord = await logger.asyncTask(
+          'get-urls',
+          async (): Promise<FigmaImagesRecord> => {
+            return getFigmaImages({
+              token: figmaAPIToken,
+              file_key: figmaSourceFileKey,
+              ids: svgsToLoad.flatMap(({ base, cutouts }: SVGToLoad): readonly string[] => {
+                return [base.id, ...cutouts.map((cutout: SVGInnerShape): string => cutout.id)];
+              }),
+            });
+          },
+        );
 
-              const positionedBaseSvgContent: string = wrapWithSvgTranslation(
-                extractSvgContent(baseSvg),
-                base.x - x,
-                base.y - y,
-              );
+        await logger.asyncTask('store-svgs', async (logger: Logger): Promise<void> => {
+          await Promise.all(
+            svgsToLoad.map(
+              async ({ name, x, y, width, height, base, cutouts }: SVGToLoad): Promise<void> => {
+                logger.info(name);
 
-              const positionedBaseSvgContentForMask: string = replaceSvgFillAndStroke(
-                positionedBaseSvgContent,
-                'white',
-              );
+                // get the svg content for the base and cutouts
+                const [baseSvg, ...cutoutSvgs] = await Promise.all([
+                  fetchFigmaSvgAsset(images[base.id]),
+                  ...cutouts.map((cutout: SVGInnerShape): Promise<string> => {
+                    return fetchFigmaSvgAsset(images[cutout.id]);
+                  }),
+                ]);
 
-              const positionedCutoutSvgContentsForMask: string = cutouts
-                .map((cutout: SVGInnerShape, index: number) => {
-                  return wrapWithSvgTranslation(
-                    replaceSvgFillAndStroke(extractSvgContent(cutoutSvgs[index]), 'black'),
-                    cutout.x - x,
-                    cutout.y - y,
-                  );
-                })
-                .join('\n');
+                // create a unique mask id
+                const maskId: string = `mask_${name.replace(/\W/g, '_')}`;
 
-              const svg: string = dedent`
+                // create the "base" svg content
+                const positionedBaseSvgContent: string = wrapWithSvgTranslation(
+                  extractSvgContent(baseSvg),
+                  base.x - x,
+                  base.y - y,
+                );
+
+                // create the "base" svg content for the mask => it's the same as the "base" one, but with stroke and fill beeing white.
+                const positionedBaseSvgContentForMask: string = replaceSvgFillAndStroke(
+                  positionedBaseSvgContent,
+                  'white',
+                );
+
+                // create the "cutouts" svg content for the mask => the "black" parts that are subtracted in the mask
+                const positionedCutoutSvgContentsForMask: string = cutouts
+                  .map((cutout: SVGInnerShape, index: number) => {
+                    return wrapWithSvgTranslation(
+                      replaceSvgFillAndStroke(extractSvgContent(cutoutSvgs[index]), 'black'),
+                      cutout.x - x,
+                      cutout.y - y,
+                    );
+                  })
+                  .join('\n');
+
+                // assemble the final svg
+                const svg: string = dedent`
                 <svg width="${String(width)}" height="${String(height)}" viewBox="0 0 ${String(width)} ${String(height)}" xmlns="http://www.w3.org/2000/svg">
                   <defs>
                     <mask id="${maskId}" fill="transparent">
@@ -259,14 +272,13 @@ export function extractSvgFilesFromFigmaDesignFile({
                 </svg>
               `;
 
-              logger.info(name);
-
-              await writeTextFileSafe(join(outputDirectory, `${name}.mask.svg`), svg);
-            },
-          ),
-        );
+                await writeTextFileSafe(join(outputDirectory, `${name}.mask.svg`), svg);
+              },
+            ),
+          );
+        });
       });
-    });
+    }
   });
 }
 
@@ -312,6 +324,9 @@ function figmaNodeToSVGInnerShape(
   };
 }
 
+/**
+ * Translates a svg content, wrapping it in a "translate" group.
+ */
 function wrapWithSvgTranslation(content: string, x: number, y: number): string {
   return dedent`
     <g transform="translate(${String(x)} ${String(y)})">
@@ -320,12 +335,18 @@ function wrapWithSvgTranslation(content: string, x: number, y: number): string {
   `;
 }
 
+/**
+ * Replaces the stroke and fill attributes with another value.
+ */
 function replaceSvgFillAndStroke(content: string, value: string): string {
   return content
     .replace(/stroke="[^"]*"/g, `stroke="${value}"`)
     .replace(/fill="[^"]*"/g, `fill="${value}"`);
 }
 
+/**
+ * Extracts the content of an svg (removes the `<svg>` tags)
+ */
 function extractSvgContent(svg: string): string {
   svg = svg.trim();
 
