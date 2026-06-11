fbe67c74dacac58da888f1096e0d51cc669e0c1b
lifaon74 <valentinrich@gmail.com>
2026-06-11 07:57:58 +0200

    refactor(tokens): remove leading blank space in file name

--- packages/tokens/scripts/shared/dtcg/resolver/to/css/token/types/base/font-weight/value/font-weight-design-tokens-collection-token-value-to-css-value.ts
+++ packages/tokens/scripts/shared/dtcg/resolver/to/css/token/types/base/font-weight/value/font-weight-design-tokens-collection-token-value-to-css-value.ts
@@ -1,6 +1,6 @@
 import { isNumberFontWeightDesignTokenValue } from '../../../../../../../../design-token/token/types/base/types/font-weight/value/types/number/is-number-font-weight-design-token-value.ts';
 import type { FontWeightDesignTokensCollectionTokenValue } from '../../../../../../../token/types/base/font-weight/value/font-weight-design-tokens-collection-token-value.ts';
-import { numberFontWeightDesignTokenValueToCssValue } from './types/number/ number-font-weight-design-token-value-to-css-value.ts';
+import { numberFontWeightDesignTokenValueToCssValue } from './types/number/number-font-weight-design-token-value-to-css-value.ts';
 import { predefinedFontWeightDesignTokenValueToCssValue } from './types/predefined/predefined-font-weight-design-token-value-to-css-value.ts';
 
 /**
--- packages/tokens/scripts/shared/dtcg/resolver/to/kotlin/token/types/base/font-weight/value/font-weight-design-tokens-collection-token-value-to-kotlin-value.ts
+++ packages/tokens/scripts/shared/dtcg/resolver/to/kotlin/token/types/base/font-weight/value/font-weight-design-tokens-collection-token-value-to-kotlin-value.ts
@@ -1,7 +1,7 @@
 import { isNumberFontWeightDesignTokenValue } from '../../../../../../../../design-token/token/types/base/types/font-weight/value/types/number/is-number-font-weight-design-token-value.ts';
 import type { FontWeightDesignTokensCollectionTokenValue } from '../../../../../../../token/types/base/font-weight/value/font-weight-design-tokens-collection-token-value.ts';
 import type { KotlinVariableDeclarationFontWeightValue } from '../../../../../kotlin-variable-declaration/value/built-in/font-weight/kotlin-variable-declaration-font-weight-value.ts';
-import { numberFontWeightDesignTokenValueToKotlinValue } from './types/number/ number-font-weight-design-token-value-to-kotlin-value.ts';
+import { numberFontWeightDesignTokenValueToKotlinValue } from './types/number/number-font-weight-design-token-value-to-kotlin-value.ts';
 import { predefinedFontWeightDesignTokenValueToKotlinValue } from './types/predefined/predefined-font-weight-design-token-value-to-kotlin-value.ts';
 
 export function fontWeightDesignTokensCollectionTokenValueToKotlinValue(
