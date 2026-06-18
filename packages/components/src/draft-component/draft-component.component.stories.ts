b37b704d2b8bc8cb57de70913f4d22b4316973c2
Victor Jolissaint <victor.jolissaint@infomaniak.com>
2026-06-18 09:25:35 +0200

    chore: yarn format

--- packages/components/src/draft-component/draft-component.component.stories.ts
+++ packages/components/src/draft-component/draft-component.component.stories.ts
@@ -2,7 +2,8 @@ import type { Meta, StoryObj } from '@storybook/web-components-vite';
 import { getStorybookHelpers } from '@wc-toolkit/storybook-helpers';
 import { DraftComponentComponent } from './draft-component.component.ts';
 
-const { args, argTypes, template } = getStorybookHelpers<DraftComponentComponent>('draft-component');
+const { args, argTypes, template } =
+  getStorybookHelpers<DraftComponentComponent>('draft-component');
 
 const meta = {
   title: 'Components/Draft Component',
