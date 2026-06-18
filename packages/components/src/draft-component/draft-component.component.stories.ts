50412d2d7a723e52bf30a42dbedde735569f3037
Victor Jolissaint <victor.jolissaint@infomaniak.com>
2026-06-18 09:21:20 +0200

    feat: add draft component for testing

---
+++ packages/components/src/draft-component/draft-component.component.stories.ts
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/web-components-vite';
+import { getStorybookHelpers } from '@wc-toolkit/storybook-helpers';
+import { DraftComponentComponent } from './draft-component.component.ts';
+
+const { args, argTypes, template } = getStorybookHelpers<DraftComponentComponent>('draft-component');
+
+const meta = {
+  title: 'Components/Draft Component',
+  component: 'draft-component',
+  tags: ['autodocs'],
+  args,
+  argTypes,
+  render: (args) => template(args),
+} satisfies Meta<DraftComponentComponent>;
+
+export default meta;
+
+type Story = StoryObj<DraftComponentComponent>;
+
+export const Default: Story = {
+  args: {
+    label: 'Draft Component',
+  },
+};
--- packages/components/src/public-api.ts
+++ packages/components/src/public-api.ts
@@ -1,1 +1,2 @@
+export * from './draft-component/draft-component.component.ts';
 export * from './esds-icon/esds-icon.component.ts';
