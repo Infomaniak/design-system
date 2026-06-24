7907c17f751c60cab7649ec6deec9aec8bbd2d99
Jordan Chapelle <jordan.chapelle@infomaniak.com>
2026-06-24 16:43:19 +0200

    fix: Format

--- packages/tokens/scripts/scripts/build-tokens/src/build/outputs/swift/swift-tokens-format.ts
+++ packages/tokens/scripts/scripts/build-tokens/src/build/outputs/swift/swift-tokens-format.ts
@@ -1,7 +1,7 @@
-import { segmentsToSwiftIdentifier } from "../../../../../../shared/dtcg/resolver/to/swift/token/name/design-token-name-segments-reference-to-swift-name.ts";
-import type { GenericDesignTokensCollectionToken } from "../../../../../../shared/dtcg/resolver/token/design-tokens-collection-token.ts";
-import { capitalizeFirstLetter } from "../../../../../../../../../scripts/helpers/misc/case/capitalize-first-letter/capitalize-first-letter.ts";
-import { enumPrefix } from "./CONSTANTS.ts";
+import { capitalizeFirstLetter } from '../../../../../../../../../scripts/helpers/misc/case/capitalize-first-letter/capitalize-first-letter.ts';
+import { segmentsToSwiftIdentifier } from '../../../../../../shared/dtcg/resolver/to/swift/token/name/design-token-name-segments-reference-to-swift-name.ts';
+import type { GenericDesignTokensCollectionToken } from '../../../../../../shared/dtcg/resolver/token/design-tokens-collection-token.ts';
+import { enumPrefix } from './CONSTANTS.ts';
 
 export function getTokenGroupName(token: GenericDesignTokensCollectionToken): string {
     return `${enumPrefix}${capitalizeFirstLetter(token.name[0])}`;
@@ -11,5 +11,5 @@ export function tokenToSwiftValue(prefix: string, token: GenericDesignTokensColl
     const groupName = getTokenGroupName(token);
     const tokenName = segmentsToSwiftIdentifier(token.name, 1);
 
-    return `${prefix}.${groupName}.${tokenName}`
+    return `${prefix}.${groupName}.${tokenName}`;
 }
