3aab6bac74673833cb5302bb5f1b63519bb1801e
Jordan Chapelle <jordan.chapelle@infomaniak.com>
2026-06-25 16:06:51 +0200

    chore: Remove dead code

--- packages/tokens/scripts/shared/dtcg/resolver/to/swift/token/name/clean-swift-name-segment.ts
+++
@@ -1,5 +0,0 @@
-export function cleanSwiftNameSegment(segment: string): string {
-  return segment
-    .replace(/[^a-zA-Z0-9-]/g, '')
-    .replace(/-([a-zA-Z0-9])/g, (_, letter: string) => letter.toUpperCase());
-}
