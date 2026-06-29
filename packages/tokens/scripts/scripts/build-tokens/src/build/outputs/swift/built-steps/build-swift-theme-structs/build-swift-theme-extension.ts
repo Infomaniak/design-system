a48e30e30026e3c273d3e8b601cae3e153d324de
Jordan Chapelle <jordan.chapelle@infomaniak.com>
2026-06-29 15:11:07 +0200

    chore: Add todo for later optimization

--- packages/tokens/scripts/scripts/build-tokens/src/build/outputs/swift/built-steps/build-swift-theme-structs/build-swift-theme-extension.ts
+++ packages/tokens/scripts/scripts/build-tokens/src/build/outputs/swift/built-steps/build-swift-theme-structs/build-swift-theme-extension.ts
@@ -47,7 +47,7 @@ function emitDiffNode(
 
   const canonicalFields = getSortedSwiftVariables(treeNode, modifierValueMap, path);
 
-  for (const field of canonicalFields) {
+  for (const field of canonicalFields) { // TODO: Possible optimization: Use a Map for treeNode entries to avoid O(n) search for each field
     const entry = Object.entries(treeNode).find(([k]) => toSwiftVariableName([k]) === field.name);
 
     if (!entry) continue;
