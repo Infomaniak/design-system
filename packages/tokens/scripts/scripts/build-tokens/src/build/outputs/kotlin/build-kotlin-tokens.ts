e550ae28b43c7153f5e821cacd903e1130a62f08
valentin richard <valentinrich@gmail.com>
2026-06-30 09:04:32 +0200

    Update packages/tokens/scripts/scripts/build-tokens/src/build/outputs/kotlin/build-kotlin-tokens.ts
    Co-authored-by: Victor Jolissaint <victor.jolissaint@infomaniak.com>

--- packages/tokens/scripts/scripts/build-tokens/src/build/outputs/kotlin/build-kotlin-tokens.ts
+++ packages/tokens/scripts/scripts/build-tokens/src/build/outputs/kotlin/build-kotlin-tokens.ts
@@ -437,7 +437,7 @@ function filterT2T3Tokens(token: GenericDesignTokensCollectionToken): boolean {
     token.files.some((path: string): boolean => {
       return path.includes(T2_DIRECTORY_NAME) || path.includes(T3_DIRECTORY_NAME);
     }) &&
-    /* TODO: kotlin does not support more than 248 properties, thus, we skipp the colors -> remove in the future */ token.name.at(
+    /* TODO: Kotlin does not support more than 248 properties, thus, we skip the colors -> remove in the future */ token.name.at(
       0,
     ) !== 'color'
   );
