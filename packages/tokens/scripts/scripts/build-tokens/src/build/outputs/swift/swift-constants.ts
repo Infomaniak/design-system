dc86fac35175f95ae9287303f31fa1ace6c36c61
Jordan Chapelle <jordan.chapelle@infomaniak.com>
2026-07-01 16:10:18 +0200

    chore: Fix conflict

--- packages/tokens/scripts/scripts/build-tokens/src/build/outputs/swift/swift-constants.ts
+++ packages/tokens/scripts/scripts/build-tokens/src/build/outputs/swift/swift-constants.ts
@@ -1,6 +1,5 @@
 export const SWIFT_RAW_TOKENS_PREFIX = 'RawToken';
 export const SWIFT_ENUM_PREFIX = 'Esds';
-export const SWIFT_STRUCT_PREFIX = 'Esds';
-export const SWIFT_MAIN_STRUCT = `${SWIFT_STRUCT_PREFIX}Theme`;
+export const SWIFT_MAIN_STRUCT = `ESDSTheme`;
 export const SWIFT_SOURCES_DIR = 'Sources/DesignSystem';
 export const SWIFT_RESOURCES_DIR = 'Resources';
