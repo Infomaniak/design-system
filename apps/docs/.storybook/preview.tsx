84bee3a201fd92ab0d3d36e736dc74676df004d3
Victor Jolissaint <victor.jolissaint@infomaniak.com>
2026-07-01 10:23:43 +0200

    chore: yarn format

--- apps/docs/.storybook/preview.tsx
+++ apps/docs/.storybook/preview.tsx
@@ -1,3 +1,8 @@
+import {
+  EsdsIconComponent,
+  ICONIFY_API,
+  InjectionContext,
+} from '@infomaniak-design-system/components';
 import { DocsContainer, type DocsContainerProps } from '@storybook/addon-docs/blocks';
 import { setCustomElementsManifest } from '@storybook/web-components';
 import type { Preview } from '@storybook/web-components-vite';
@@ -8,11 +13,6 @@ import { Globals, GlobalsUpdatedPayload } from 'storybook/internal/types';
 import customElements from '../../../packages/components/custom-elements.json' with { type: 'json' };
 import MaterialThemeBuilderLink from '../src/components/MaterialThemeBuilderLink.tsx';
 import Table from '../src/components/Table.tsx';
-import {
-  EsdsIconComponent,
-  ICONIFY_API,
-  InjectionContext,
-} from '@infomaniak-design-system/components';
 import { iconifyApi } from '../src/lib/iconify-api.ts';
 
 setCustomElementsManifest(customElements);
@@ -54,9 +54,7 @@ import '@infomaniak-design-system/tokens/dist/web/css/material/modifiers/theme/l
 // Initialize <esds-icon> and provide IconifyApi via root InjectionContext
 EsdsIconComponent.define();
 
-InjectionContext.root = new InjectionContext([
-  ICONIFY_API.define(iconifyApi),
-]);
+InjectionContext.root = new InjectionContext([ICONIFY_API.define(iconifyApi)]);
 
 /**
  * Utility function to set data attributes on document.body for CSS theming
