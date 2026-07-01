7210b57031d2bd1fd83aea6708dcda6239e1bc7a
Jordan Chapelle <jordan.chapelle@infomaniak.com>
2026-07-01 14:44:03 +0200

    feat(swift): rename main struct from EsdsTheme to ESDSTheme

--- packages/tokens/scripts/scripts/build-tokens/src/build/outputs/swift/swift-constants.ts
+++ packages/tokens/scripts/scripts/build-tokens/src/build/outputs/swift/swift-constants.ts
@@ -1,4 +1,3 @@
 export const SWIFT_RAW_TOKENS_PREFIX = 'RawToken';
 export const SWIFT_ENUM_PREFIX = 'Esds';
-export const SWIFT_STRUCT_PREFIX = 'Esds';
-export const SWIFT_MAIN_STRUCT = `${SWIFT_STRUCT_PREFIX}Theme`;
+export const SWIFT_MAIN_STRUCT = 'ESDSTheme';
