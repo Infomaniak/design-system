39549d8eea7a08e261a1dbea45930f8b43376698
vjo <vjo@users.noreply.github.com>
2026-07-15 13:53:22 +0000

    deploy: c8f4b65d582f2a744fd185027a5af4889b7140d6

---
+++ storybook/mr/245/assets/shadow-BuUo8fLB.js
@@ -0,0 +1,294 @@
+import{i as e}from"./preload-helper-CT_b8DTk.js";import{k as t,l as n,m as r,u as i}from"./iframe-DezdfMpy.js";import{t as a}from"./mdx-react-shim-CYRRLyV4.js";import{n as o,t as s}from"./dist-Bj5dcNbH.js";var c,l=e((()=>{c=`<!-- Do not edit directly, this file was auto-generated. -->
+
+<table class="token-table">
+  <thead>
+    <tr>
+      <th style="width: 20%;">Preview</th>
+      <th style="width: 80%;">Details</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-shadow-0-x); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-shadow-0-x"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="shadow.0.x" type="button">shadow.0.x</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-shadow-0-x)" type="button">var(--esds-shadow-0-x)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-shadow-0-y); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-shadow-0-y"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="shadow.0.y" type="button">shadow.0.y</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-shadow-0-y)" type="button">var(--esds-shadow-0-y)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-shadow-0-blur); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-shadow-0-blur"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="shadow.0.blur" type="button">shadow.0.blur</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-shadow-0-blur)" type="button">var(--esds-shadow-0-blur)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-shadow-0-spread); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-shadow-0-spread"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="shadow.0.spread" type="button">shadow.0.spread</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-shadow-0-spread)" type="button">var(--esds-shadow-0-spread)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-shadow-1-x); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-shadow-1-x"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="shadow.1.x" type="button">shadow.1.x</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-shadow-1-x)" type="button">var(--esds-shadow-1-x)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-shadow-1-y); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-shadow-1-y"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="shadow.1.y" type="button">shadow.1.y</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-shadow-1-y)" type="button">var(--esds-shadow-1-y)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-shadow-1-blur); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-shadow-1-blur"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="shadow.1.blur" type="button">shadow.1.blur</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-shadow-1-blur)" type="button">var(--esds-shadow-1-blur)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-shadow-1-spread); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-shadow-1-spread"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="shadow.1.spread" type="button">shadow.1.spread</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-shadow-1-spread)" type="button">var(--esds-shadow-1-spread)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-shadow-2-x); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-shadow-2-x"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="shadow.2.x" type="button">shadow.2.x</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-shadow-2-x)" type="button">var(--esds-shadow-2-x)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-shadow-2-y); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-shadow-2-y"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="shadow.2.y" type="button">shadow.2.y</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-shadow-2-y)" type="button">var(--esds-shadow-2-y)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-shadow-2-blur); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-shadow-2-blur"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="shadow.2.blur" type="button">shadow.2.blur</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-shadow-2-blur)" type="button">var(--esds-shadow-2-blur)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-shadow-2-spread); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-shadow-2-spread"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="shadow.2.spread" type="button">shadow.2.spread</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-shadow-2-spread)" type="button">var(--esds-shadow-2-spread)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-shadow-3-x); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-shadow-3-x"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="shadow.3.x" type="button">shadow.3.x</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-shadow-3-x)" type="button">var(--esds-shadow-3-x)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-shadow-3-y); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-shadow-3-y"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="shadow.3.y" type="button">shadow.3.y</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-shadow-3-y)" type="button">var(--esds-shadow-3-y)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-shadow-3-blur); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-shadow-3-blur"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="shadow.3.blur" type="button">shadow.3.blur</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-shadow-3-blur)" type="button">var(--esds-shadow-3-blur)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-shadow-3-spread); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-shadow-3-spread"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="shadow.3.spread" type="button">shadow.3.spread</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-shadow-3-spread)" type="button">var(--esds-shadow-3-spread)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-shadow-4-x); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-shadow-4-x"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="shadow.4.x" type="button">shadow.4.x</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-shadow-4-x)" type="button">var(--esds-shadow-4-x)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-shadow-4-y); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-shadow-4-y"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="shadow.4.y" type="button">shadow.4.y</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-shadow-4-y)" type="button">var(--esds-shadow-4-y)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-shadow-4-blur); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-shadow-4-blur"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="shadow.4.blur" type="button">shadow.4.blur</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-shadow-4-blur)" type="button">var(--esds-shadow-4-blur)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-shadow-4-spread); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-shadow-4-spread"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="shadow.4.spread" type="button">shadow.4.spread</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-shadow-4-spread)" type="button">var(--esds-shadow-4-spread)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-shadow-5-x); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-shadow-5-x"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="shadow.5.x" type="button">shadow.5.x</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-shadow-5-x)" type="button">var(--esds-shadow-5-x)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-shadow-5-y); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-shadow-5-y"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="shadow.5.y" type="button">shadow.5.y</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-shadow-5-y)" type="button">var(--esds-shadow-5-y)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-shadow-5-blur); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-shadow-5-blur"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="shadow.5.blur" type="button">shadow.5.blur</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-shadow-5-blur)" type="button">var(--esds-shadow-5-blur)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-shadow-5-spread); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-shadow-5-spread"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="shadow.5.spread" type="button">shadow.5.spread</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-shadow-5-spread)" type="button">var(--esds-shadow-5-spread)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-shadow-6-x); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-shadow-6-x"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="shadow.6.x" type="button">shadow.6.x</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-shadow-6-x)" type="button">var(--esds-shadow-6-x)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-shadow-6-y); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-shadow-6-y"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="shadow.6.y" type="button">shadow.6.y</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-shadow-6-y)" type="button">var(--esds-shadow-6-y)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-shadow-6-blur); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-shadow-6-blur"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="shadow.6.blur" type="button">shadow.6.blur</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-shadow-6-blur)" type="button">var(--esds-shadow-6-blur)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-shadow-6-spread); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-shadow-6-spread"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="shadow.6.spread" type="button">shadow.6.spread</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-shadow-6-spread)" type="button">var(--esds-shadow-6-spread)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+  </tbody>
+</table>`}));function u(e){let t={h1:`h1`,...r(),...e.components};return(0,f.jsxs)(f.Fragment,{children:[(0,f.jsx)(n,{title:`Design Tokens/T1/Shadow`}),`
+`,(0,f.jsx)(t.h1,{id:`t1-shadow-tokens`,children:`T1 Shadow Tokens`}),`
+`,(0,f.jsx)(o,{children:c})]})}function d(e={}){let{wrapper:t}={...r(),...e.components};return t?(0,f.jsx)(t,{...e,children:(0,f.jsx)(u,{...e})}):u(e)}var f;e((()=>{f=t(),a(),i(),l(),s()}))();export{d as default};
