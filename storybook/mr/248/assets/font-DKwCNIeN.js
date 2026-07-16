859a2fc39d285ffec66167d84b52149f7b228f22
vjo <vjo@users.noreply.github.com>
2026-07-16 14:08:10 +0000

    deploy: 7f61fe40bd837c5e0f6e26b68587960794f6e477

---
+++ storybook/mr/248/assets/font-DKwCNIeN.js
@@ -0,0 +1,234 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{o as t,s as n}from"./blocks-BAdpgMgA.js";import{a as r}from"./chunk-W22LQPXL-DcDnD_CV.js";import{i}from"./react-XnqUzw--.js";import{t as a}from"./mdx-react-shim-y1jXGhTh.js";import{n as o,t as s}from"./dist-Bv6Zr5sR.js";var c,l=e((()=>{c=`<!-- Do not edit directly, this file was auto-generated. -->
+
+<table class="token-table">
+  <thead>
+    <tr>
+      <th style="width: 20%;">Preview</th>
+      <th style="width: 80%;">Details</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td><p style=" font-size: var(--esds-font-size-12); margin: 0; padding: 8px; background: #f9fafb; border-radius: 4px; border: 1px solid #e5e7eb; line-height: 1.4; " > Edelweiss prefers rocky limestone locations </p> <div data-preview-value="--esds-font-size-12"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="font.size.12" type="button">font.size.12</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-font-size-12)" type="button">var(--esds-font-size-12)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><p style=" font-size: var(--esds-font-size-14); margin: 0; padding: 8px; background: #f9fafb; border-radius: 4px; border: 1px solid #e5e7eb; line-height: 1.4; " > Edelweiss prefers rocky limestone locations </p> <div data-preview-value="--esds-font-size-14"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="font.size.14" type="button">font.size.14</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-font-size-14)" type="button">var(--esds-font-size-14)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><p style=" font-size: var(--esds-font-size-16); margin: 0; padding: 8px; background: #f9fafb; border-radius: 4px; border: 1px solid #e5e7eb; line-height: 1.4; " > Edelweiss prefers rocky limestone locations </p> <div data-preview-value="--esds-font-size-16"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="font.size.16" type="button">font.size.16</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-font-size-16)" type="button">var(--esds-font-size-16)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><p style=" font-size: var(--esds-font-size-18); margin: 0; padding: 8px; background: #f9fafb; border-radius: 4px; border: 1px solid #e5e7eb; line-height: 1.4; " > Edelweiss prefers rocky limestone locations </p> <div data-preview-value="--esds-font-size-18"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="font.size.18" type="button">font.size.18</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-font-size-18)" type="button">var(--esds-font-size-18)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><p style=" font-size: var(--esds-font-size-20); margin: 0; padding: 8px; background: #f9fafb; border-radius: 4px; border: 1px solid #e5e7eb; line-height: 1.4; " > Edelweiss prefers rocky limestone locations </p> <div data-preview-value="--esds-font-size-20"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="font.size.20" type="button">font.size.20</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-font-size-20)" type="button">var(--esds-font-size-20)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><p style=" font-size: var(--esds-font-size-24); margin: 0; padding: 8px; background: #f9fafb; border-radius: 4px; border: 1px solid #e5e7eb; line-height: 1.4; " > Edelweiss prefers rocky limestone locations </p> <div data-preview-value="--esds-font-size-24"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="font.size.24" type="button">font.size.24</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-font-size-24)" type="button">var(--esds-font-size-24)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><p style=" font-size: var(--esds-font-size-32); margin: 0; padding: 8px; background: #f9fafb; border-radius: 4px; border: 1px solid #e5e7eb; line-height: 1.4; " > Edelweiss prefers rocky limestone locations </p> <div data-preview-value="--esds-font-size-32"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="font.size.32" type="button">font.size.32</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-font-size-32)" type="button">var(--esds-font-size-32)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><p style=" font-size: var(--esds-font-size-40); margin: 0; padding: 8px; background: #f9fafb; border-radius: 4px; border: 1px solid #e5e7eb; line-height: 1.4; " > Edelweiss prefers rocky limestone locations </p> <div data-preview-value="--esds-font-size-40"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="font.size.40" type="button">font.size.40</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-font-size-40)" type="button">var(--esds-font-size-40)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><p style=" font-size: var(--esds-font-size-48); margin: 0; padding: 8px; background: #f9fafb; border-radius: 4px; border: 1px solid #e5e7eb; line-height: 1.4; " > Edelweiss prefers rocky limestone locations </p> <div data-preview-value="--esds-font-size-48"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="font.size.48" type="button">font.size.48</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-font-size-48)" type="button">var(--esds-font-size-48)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><p style=" font-weight: var(--esds-font-weight-400); font-size: 16px; font-family: system-ui, sans-serif; margin: 0; padding: 8px; background: #f9fafb; border-radius: 4px; border: 1px solid #e5e7eb; " > Edelweiss prefers rocky limestone locations </p> <div data-preview-value="--esds-font-weight-400"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="font.weight.400" type="button">font.weight.400</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-font-weight-400)" type="button">var(--esds-font-weight-400)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><p style=" font-weight: var(--esds-font-weight-500); font-size: 16px; font-family: system-ui, sans-serif; margin: 0; padding: 8px; background: #f9fafb; border-radius: 4px; border: 1px solid #e5e7eb; " > Edelweiss prefers rocky limestone locations </p> <div data-preview-value="--esds-font-weight-500"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="font.weight.500" type="button">font.weight.500</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-font-weight-500)" type="button">var(--esds-font-weight-500)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><p style=" letter-spacing: var(--esds-font-letter-spacing-0); font-size: 16px; margin: 0; padding: 8px; background: #f9fafb; border-radius: 4px; border: 1px solid #e5e7eb; line-height: 1.4; " > Edelweiss prefers rocky limestone locations </p> <div data-preview-value="--esds-font-letter-spacing-0"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="font.letter-spacing.0" type="button">font.letter-spacing.0</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-font-letter-spacing-0)" type="button">var(--esds-font-letter-spacing-0)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-font-line-height-16); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-font-line-height-16"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="font.line-height.16" type="button">font.line-height.16</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-font-line-height-16)" type="button">var(--esds-font-line-height-16)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-font-line-height-20); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-font-line-height-20"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="font.line-height.20" type="button">font.line-height.20</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-font-line-height-20)" type="button">var(--esds-font-line-height-20)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-font-line-height-24); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-font-line-height-24"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="font.line-height.24" type="button">font.line-height.24</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-font-line-height-24)" type="button">var(--esds-font-line-height-24)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-font-line-height-26); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-font-line-height-26"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="font.line-height.26" type="button">font.line-height.26</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-font-line-height-26)" type="button">var(--esds-font-line-height-26)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-font-line-height-28); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-font-line-height-28"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="font.line-height.28" type="button">font.line-height.28</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-font-line-height-28)" type="button">var(--esds-font-line-height-28)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-font-line-height-32); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-font-line-height-32"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="font.line-height.32" type="button">font.line-height.32</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-font-line-height-32)" type="button">var(--esds-font-line-height-32)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-font-line-height-40); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-font-line-height-40"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="font.line-height.40" type="button">font.line-height.40</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-font-line-height-40)" type="button">var(--esds-font-line-height-40)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-font-line-height-48); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-font-line-height-48"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="font.line-height.48" type="button">font.line-height.48</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-font-line-height-48)" type="button">var(--esds-font-line-height-48)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-font-line-height-56); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-font-line-height-56"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="font.line-height.56" type="button">font.line-height.56</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-font-line-height-56)" type="button">var(--esds-font-line-height-56)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><p style=" font-family: var(--esds-font-family-suisse-int-l); font-size: 16px; margin: 0; padding: 8px; background: #f9fafb; border-radius: 4px; border: 1px solid #e5e7eb; " > Edelweiss prefers rocky limestone locations </p> <div style=" margin-top: 4px; font-family: monospace; font-size: 12px; color: #6b7280; " > "Suisse Int'l" </div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="font.family.Suisse Int'l" type="button">font.family.Suisse Int'l</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-font-family-suisse-int-l)" type="button">var(--esds-font-family-suisse-int-l)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+  </tbody>
+</table>`}));function u(e){let n={h1:`h1`,...i(),...e.components};return(0,f.jsxs)(f.Fragment,{children:[(0,f.jsx)(t,{title:`Design Tokens/T1/Font`}),`
+`,(0,f.jsx)(n.h1,{id:`t1-font-tokens`,children:`T1 Font Tokens`}),`
+`,(0,f.jsx)(o,{children:c})]})}function d(e={}){let{wrapper:t}={...i(),...e.components};return t?(0,f.jsx)(t,{...e,children:(0,f.jsx)(u,{...e})}):u(e)}var f;e((()=>{f=r(),a(),n(),l(),s()}))();export{d as default};
