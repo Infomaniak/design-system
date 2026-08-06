2c9b620800782b026bd8eb5772e5226c7260413c
lifaon74 <lifaon74@users.noreply.github.com>
2026-08-06 11:49:36 +0000

    deploy: 8c8ce92bb488961c88b166567cd0bc381b498856

---
+++ storybook/mr/289/assets/radius-CdLK-iGo.js
@@ -0,0 +1,114 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{o as t,s as n}from"./blocks-DF7plsZK.js";import{a as r}from"./chunk-W22LQPXL-D4r6JC00.js";import{i,r as a}from"./react-Bl2r1tuC.js";import{n as o,t as s}from"./dist-C80e1hHm.js";var c;function l(){return(l=e((()=>{c=`<!-- Do not edit directly, this file was auto-generated. -->
+
+<table class="token-table">
+  <thead>
+    <tr>
+      <th style="width: 20%;">Preview</th>
+      <th style="width: 80%;">Details</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td><div style=" width: 100px; height: 100px; background: #dcfce8; border: 2px solid #374151; border-radius: var(--esds-radius-0); display: inline-block; " ></div> <div data-preview-value="--esds-radius-0"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="radius.0" type="button">radius.0</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-radius-0)" type="button">var(--esds-radius-0)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" width: 100px; height: 100px; background: #dcfce8; border: 2px solid #374151; border-radius: var(--esds-radius-2); display: inline-block; " ></div> <div data-preview-value="--esds-radius-2"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="radius.2" type="button">radius.2</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-radius-2)" type="button">var(--esds-radius-2)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" width: 100px; height: 100px; background: #dcfce8; border: 2px solid #374151; border-radius: var(--esds-radius-4); display: inline-block; " ></div> <div data-preview-value="--esds-radius-4"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="radius.4" type="button">radius.4</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-radius-4)" type="button">var(--esds-radius-4)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" width: 100px; height: 100px; background: #dcfce8; border: 2px solid #374151; border-radius: var(--esds-radius-6); display: inline-block; " ></div> <div data-preview-value="--esds-radius-6"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="radius.6" type="button">radius.6</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-radius-6)" type="button">var(--esds-radius-6)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" width: 100px; height: 100px; background: #dcfce8; border: 2px solid #374151; border-radius: var(--esds-radius-8); display: inline-block; " ></div> <div data-preview-value="--esds-radius-8"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="radius.8" type="button">radius.8</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-radius-8)" type="button">var(--esds-radius-8)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" width: 100px; height: 100px; background: #dcfce8; border: 2px solid #374151; border-radius: var(--esds-radius-12); display: inline-block; " ></div> <div data-preview-value="--esds-radius-12"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="radius.12" type="button">radius.12</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-radius-12)" type="button">var(--esds-radius-12)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" width: 100px; height: 100px; background: #dcfce8; border: 2px solid #374151; border-radius: var(--esds-radius-16); display: inline-block; " ></div> <div data-preview-value="--esds-radius-16"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="radius.16" type="button">radius.16</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-radius-16)" type="button">var(--esds-radius-16)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" width: 100px; height: 100px; background: #dcfce8; border: 2px solid #374151; border-radius: var(--esds-radius-24); display: inline-block; " ></div> <div data-preview-value="--esds-radius-24"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="radius.24" type="button">radius.24</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-radius-24)" type="button">var(--esds-radius-24)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" width: 100px; height: 100px; background: #dcfce8; border: 2px solid #374151; border-radius: var(--esds-radius-32); display: inline-block; " ></div> <div data-preview-value="--esds-radius-32"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="radius.32" type="button">radius.32</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-radius-32)" type="button">var(--esds-radius-32)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" width: 100px; height: 100px; background: #dcfce8; border: 2px solid #374151; border-radius: var(--esds-radius-1000); display: inline-block; " ></div> <div data-preview-value="--esds-radius-1000"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="radius.1000" type="button">radius.1000</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-radius-1000)" type="button">var(--esds-radius-1000)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+  </tbody>
+</table>`})))()}function u(e){let n={h1:`h1`,...i(),...e.components};return(0,f.jsxs)(f.Fragment,{children:[(0,f.jsx)(t,{title:`Design Tokens/T1/Radius`}),`
+`,(0,f.jsx)(n.h1,{id:`t1-radius-tokens`,children:`T1 Radius Tokens`}),`
+`,(0,f.jsx)(o,{children:c})]})}function d(e={}){let{wrapper:t}={...i(),...e.components};return t?(0,f.jsx)(t,{...e,children:(0,f.jsx)(u,{...e})}):u(e)}var f;function p(){return(p=e((()=>{f=r(),a(),n(),l(),s()})))()}p();export{d as default};
