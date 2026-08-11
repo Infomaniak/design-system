d998354c505c9d6a12b9205057acadd4e68f7e9a
lifaon74 <valentinrich@gmail.com>
2026-08-11 09:25:43 +0200

    feat(tokens): improve import to support descriptions

--- packages/tokens/scripts/scripts/convert-figma-tokens/convert-figma-tokens.script.ts
+++ packages/tokens/scripts/scripts/convert-figma-tokens/convert-figma-tokens.script.ts
@@ -1,12 +1,16 @@
-import { rm } from 'node:fs/promises';
-import { dirname, join } from 'node:path';
+import { glob, rm } from 'node:fs/promises';
+import { dirname, join, relative } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { readJsonFile } from '../../../../../scripts/helpers/file/read-json-file.ts';
 import { DEFAULT_LOG_LEVEL } from '../../../../../scripts/helpers/log/log-level/defaults/default-log-level.ts';
 import { Logger } from '../../../../../scripts/helpers/log/logger.ts';
 import { execCommandInherit } from '../../../../../scripts/helpers/misc/exec-command.ts';
 import {
   DESIGN_TOKEN_TIERS,
   MODIFIERS_DIRECTORY_NAME,
+  T1_DIRECTORY_NAME,
+  T2_DIRECTORY_NAME,
+  T3_DIRECTORY_NAME,
 } from '../build-tokens/src/constants/design-token-tiers.ts';
 
 import { convertFigmaTokens } from './src/convert-figma.tokens.ts';
@@ -20,12 +24,23 @@ const OUTPUT_DIR: string = join(ROOT_DIR, 'tokens');
 const logger = Logger.root({ logLevel: DEFAULT_LOG_LEVEL });
 
 export async function convertFigmaTokensScript(): Promise<void> {
+  const previousTokens = new Map();
+
+  for await (const entry of glob([
+    `${OUTPUT_DIR}/${T1_DIRECTORY_NAME}/*.tokens.json`,
+    `${OUTPUT_DIR}/${T2_DIRECTORY_NAME}/*.tokens.json`,
+    `${OUTPUT_DIR}/${T3_DIRECTORY_NAME}/*.tokens.json`,
+  ])) {
+    previousTokens.set(relative(OUTPUT_DIR, entry), await readJsonFile(entry));
+  }
+
   for (const subDirectory of [...DESIGN_TOKEN_TIERS, MODIFIERS_DIRECTORY_NAME]) {
     await rm(join(OUTPUT_DIR, subDirectory), { force: true, recursive: true });
   }
 
   await convertFigmaTokens({
     tokensPath: TOKENS_PATH,
+    previousTokens,
     outputDirectory: OUTPUT_DIR,
   });
 
