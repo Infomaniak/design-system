f40387855c3a66f4df1444e34d91e7a6db5be07d
vjo <vjo@users.noreply.github.com>
2026-08-11 11:57:34 +0000

    deploy: 4852e7f2f4ae0cd32cd003c98956fd577fc503d0

---
+++ storybook/mr/290/assets/iframe-v6n1O0_m.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-DxLmo0pj.js";e();
---
+++ storybook/mr/290/assets/spacing-Bh5w3vwv.js
@@ -0,0 +1,155 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{o as t,s as n}from"./blocks-Ik97dzc2.js";import{a as r}from"./chunk-W22LQPXL-Bced4FlR.js";import{i,r as a}from"./react-Bl2r1tuC.js";import{n as o,t as s}from"./dist-dAU-ll2Y.js";var c;function l(){return(l=e((()=>{c=`<!-- Do not edit directly, this file was auto-generated. -->
+
+<table class="token-table">
+  <thead>
+    <tr>
+      <th style="width: 20%;">Preview</th>
+      <th style="width: 80%;">Details</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-spacing-none); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-spacing-none"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="spacing.none" type="button">spacing.none</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-spacing-none)" type="button">var(--esds-spacing-none)</button>
+              </div><div class="token-row">Tailwind: <button class="token-value" data-clipboard="p-none" type="button">p-none</button> <button class="token-value" data-clipboard="m-none" type="button">m-none</button> <button class="token-value" data-clipboard="gap-none" type="button">gap-none</button></div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-spacing-2xs); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-spacing-2xs"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="spacing.2xs" type="button">spacing.2xs</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-spacing-2xs)" type="button">var(--esds-spacing-2xs)</button>
+              </div><div class="token-row">Tailwind: <button class="token-value" data-clipboard="p-2xs" type="button">p-2xs</button> <button class="token-value" data-clipboard="m-2xs" type="button">m-2xs</button> <button class="token-value" data-clipboard="gap-2xs" type="button">gap-2xs</button></div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-spacing-xs); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-spacing-xs"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="spacing.xs" type="button">spacing.xs</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-spacing-xs)" type="button">var(--esds-spacing-xs)</button>
+              </div><div class="token-row">Tailwind: <button class="token-value" data-clipboard="p-xs" type="button">p-xs</button> <button class="token-value" data-clipboard="m-xs" type="button">m-xs</button> <button class="token-value" data-clipboard="gap-xs" type="button">gap-xs</button></div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-spacing-sm); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-spacing-sm"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="spacing.sm" type="button">spacing.sm</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-spacing-sm)" type="button">var(--esds-spacing-sm)</button>
+              </div><div class="token-row">Tailwind: <button class="token-value" data-clipboard="p-sm" type="button">p-sm</button> <button class="token-value" data-clipboard="m-sm" type="button">m-sm</button> <button class="token-value" data-clipboard="gap-sm" type="button">gap-sm</button></div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-spacing-md); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-spacing-md"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="spacing.md" type="button">spacing.md</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-spacing-md)" type="button">var(--esds-spacing-md)</button>
+              </div><div class="token-row">Tailwind: <button class="token-value" data-clipboard="p-md" type="button">p-md</button> <button class="token-value" data-clipboard="m-md" type="button">m-md</button> <button class="token-value" data-clipboard="gap-md" type="button">gap-md</button></div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-spacing-lg); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-spacing-lg"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="spacing.lg" type="button">spacing.lg</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-spacing-lg)" type="button">var(--esds-spacing-lg)</button>
+              </div><div class="token-row">Tailwind: <button class="token-value" data-clipboard="p-lg" type="button">p-lg</button> <button class="token-value" data-clipboard="m-lg" type="button">m-lg</button> <button class="token-value" data-clipboard="gap-lg" type="button">gap-lg</button></div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-spacing-xl); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-spacing-xl"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="spacing.xl" type="button">spacing.xl</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-spacing-xl)" type="button">var(--esds-spacing-xl)</button>
+              </div><div class="token-row">Tailwind: <button class="token-value" data-clipboard="p-xl" type="button">p-xl</button> <button class="token-value" data-clipboard="m-xl" type="button">m-xl</button> <button class="token-value" data-clipboard="gap-xl" type="button">gap-xl</button></div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-spacing-2xl); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-spacing-2xl"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="spacing.2xl" type="button">spacing.2xl</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-spacing-2xl)" type="button">var(--esds-spacing-2xl)</button>
+              </div><div class="token-row">Tailwind: <button class="token-value" data-clipboard="p-2xl" type="button">p-2xl</button> <button class="token-value" data-clipboard="m-2xl" type="button">m-2xl</button> <button class="token-value" data-clipboard="gap-2xl" type="button">gap-2xl</button></div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-spacing-3xl); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-spacing-3xl"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="spacing.3xl" type="button">spacing.3xl</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-spacing-3xl)" type="button">var(--esds-spacing-3xl)</button>
+              </div><div class="token-row">Tailwind: <button class="token-value" data-clipboard="p-3xl" type="button">p-3xl</button> <button class="token-value" data-clipboard="m-3xl" type="button">m-3xl</button> <button class="token-value" data-clipboard="gap-3xl" type="button">gap-3xl</button></div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-spacing-4xl); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-spacing-4xl"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="spacing.4xl" type="button">spacing.4xl</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-spacing-4xl)" type="button">var(--esds-spacing-4xl)</button>
+              </div><div class="token-row">Tailwind: <button class="token-value" data-clipboard="p-4xl" type="button">p-4xl</button> <button class="token-value" data-clipboard="m-4xl" type="button">m-4xl</button> <button class="token-value" data-clipboard="gap-4xl" type="button">gap-4xl</button></div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-spacing-5xl); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-spacing-5xl"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="spacing.5xl" type="button">spacing.5xl</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-spacing-5xl)" type="button">var(--esds-spacing-5xl)</button>
+              </div><div class="token-row">Tailwind: <button class="token-value" data-clipboard="p-5xl" type="button">p-5xl</button> <button class="token-value" data-clipboard="m-5xl" type="button">m-5xl</button> <button class="token-value" data-clipboard="gap-5xl" type="button">gap-5xl</button></div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-spacing-6xl); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-spacing-6xl"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="spacing.6xl" type="button">spacing.6xl</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-spacing-6xl)" type="button">var(--esds-spacing-6xl)</button>
+              </div><div class="token-row">Tailwind: <button class="token-value" data-clipboard="p-6xl" type="button">p-6xl</button> <button class="token-value" data-clipboard="m-6xl" type="button">m-6xl</button> <button class="token-value" data-clipboard="gap-6xl" type="button">gap-6xl</button></div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-spacing-7xl); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-spacing-7xl"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="spacing.7xl" type="button">spacing.7xl</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-spacing-7xl)" type="button">var(--esds-spacing-7xl)</button>
+              </div><div class="token-row">Tailwind: <button class="token-value" data-clipboard="p-7xl" type="button">p-7xl</button> <button class="token-value" data-clipboard="m-7xl" type="button">m-7xl</button> <button class="token-value" data-clipboard="gap-7xl" type="button">gap-7xl</button></div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-spacing-8xl); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-spacing-8xl"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="spacing.8xl" type="button">spacing.8xl</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-spacing-8xl)" type="button">var(--esds-spacing-8xl)</button>
+              </div><div class="token-row">Tailwind: <button class="token-value" data-clipboard="p-8xl" type="button">p-8xl</button> <button class="token-value" data-clipboard="m-8xl" type="button">m-8xl</button> <button class="token-value" data-clipboard="gap-8xl" type="button">gap-8xl</button></div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+  </tbody>
+</table>`})))()}function u(e){let n={h1:`h1`,p:`p`,...i(),...e.components};return(0,f.jsxs)(f.Fragment,{children:[(0,f.jsx)(t,{title:`Design Tokens/T2/Spacing`}),`
+`,(0,f.jsx)(n.h1,{id:`t2-spacing-tokens`,children:`T2 Spacing Tokens`}),`
+`,(0,f.jsx)(n.p,{children:`T2 Spacing tokens provide semantic spacing definitions for margins, padding, and gaps.`}),`
+`,(0,f.jsx)(s,{children:c})]})}function d(e={}){let{wrapper:t}={...i(),...e.components};return t?(0,f.jsx)(t,{...e,children:(0,f.jsx)(u,{...e})}):u(e)}var f;function p(){return(p=e((()=>{f=r(),a(),n(),l(),o()})))()}p();export{d as default};
