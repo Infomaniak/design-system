ffcf3e7f8caddfe87f9c2c7ab52a425a6e621e07
lifaon74 <valentinrich@gmail.com>
2026-08-12 07:45:08 +0200

    chore(tokens): fix invalid function name

--- packages/tokens/scripts/shared/dtcg/resolver/token/types/base/dimension/value/convert/convert-dimension-design-tokens-collection-token-value-to-rem.ts
+++ packages/tokens/scripts/shared/dtcg/resolver/token/types/base/dimension/value/convert/convert-dimension-design-tokens-collection-token-value-to-rem.ts
@@ -1,7 +1,7 @@
 import { convertPxToRem } from '../../../../../../../design-token/token/types/base/types/dimension/value/members/value/convert/convert-px-to-rem.ts';
 import type { DimensionDesignTokensCollectionTokenValue } from '../dimension-design-tokens-collection-token-value.ts';
 
-export function convertDimensionDesignTokensCollectionTokenValueToPx({
+export function convertDimensionDesignTokensCollectionTokenValueToRem({
   value,
   unit,
 }: DimensionDesignTokensCollectionTokenValue): DimensionDesignTokensCollectionTokenValue {
