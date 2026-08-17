62ea85cf8c4da6600391d41a826660fc18e1f1b2
lifaon74 <valentinrich@gmail.com>
2026-08-17 10:54:02 +0200

    build(components): improve vite config -> make output js files match the package files structure.

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -1,5 +1,6 @@
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import type { PreRenderedChunk } from 'rolldown';
 import { defineConfig } from 'vite';
 import { viteTc39DecoratorsPlugin } from '../../plugins/vite-tc39-decorators-plugin.ts';
 
@@ -17,6 +18,25 @@ export default defineConfig({
       external: ['lit', /lit\/.*/],
       output: {
         preserveModules: true,
+        entryFileNames: (chunkInfo: PreRenderedChunk): string => {
+          let name: string = chunkInfo.name;
+
+          if (name.startsWith('node_modules/')) {
+            name = `external/${name.slice(13)}`;
+          } else if (name.startsWith('packages/components/src/')) {
+            name = name.slice(24);
+          }
+
+          {
+            const index: number = name.indexOf('?');
+
+            if (index !== -1) {
+              name = name.slice(0, index);
+            }
+          }
+
+          return `${name}.js`;
+        },
       },
     },
   },
