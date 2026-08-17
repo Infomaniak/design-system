c2b6b873b22c966527ef99cd64902dcb98abcf81
lifaon74 <lifaon74@users.noreply.github.com>
2026-08-17 10:48:47 +0000

    deploy: 1ce122486a1b90579df0c38d8515a3891cbcdadd

---
+++ storybook/mr/299/assets/blur-BBQYQkyZ.js
@@ -0,0 +1,94 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{o as t,s as n}from"./blocks-Ik97dzc2.js";import{a as r}from"./chunk-W22LQPXL-Bced4FlR.js";import{i,r as a}from"./react-Bl2r1tuC.js";import{n as o,t as s}from"./dist-dAU-ll2Y.js";var c;function l(){return(l=e((()=>{c=`<!-- Do not edit directly, this file was auto-generated. -->
+
+<table class="token-table">
+  <thead>
+    <tr>
+      <th style="width: 20%;">Preview</th>
+      <th style="width: 80%;">Details</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-blur-0); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-blur-0"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="blur.0" type="button">blur.0</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-blur-0)" type="button">var(--esds-blur-0)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-blur-4); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-blur-4"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="blur.4" type="button">blur.4</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-blur-4)" type="button">var(--esds-blur-4)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-blur-8); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-blur-8"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="blur.8" type="button">blur.8</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-blur-8)" type="button">var(--esds-blur-8)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-blur-12); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-blur-12"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="blur.12" type="button">blur.12</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-blur-12)" type="button">var(--esds-blur-12)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-blur-16); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-blur-16"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="blur.16" type="button">blur.16</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-blur-16)" type="button">var(--esds-blur-16)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-blur-24); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-blur-24"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="blur.24" type="button">blur.24</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-blur-24)" type="button">var(--esds-blur-24)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-blur-40); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-blur-40"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="blur.40" type="button">blur.40</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-blur-40)" type="button">var(--esds-blur-40)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" background: #dcfce8; height: 16px; width: var(--esds-blur-64); border-radius: 2px; border: 1px solid #86efad; position: relative; aspect-ratio: 1 / 1; " ></div> <div data-preview-value="--esds-blur-64"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="blur.64" type="button">blur.64</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-blur-64)" type="button">var(--esds-blur-64)</button>
+              </div><div class="token-row token-description"></div>
+      </td>
+    </tr>
+  </tbody>
+</table>`})))()}function u(e){let n={h1:`h1`,...i(),...e.components};return(0,f.jsxs)(f.Fragment,{children:[(0,f.jsx)(t,{title:`Design Tokens/T1/Blur`}),`
+`,(0,f.jsx)(n.h1,{id:`t1-blur-tokens`,children:`T1 Blur Tokens`}),`
+`,(0,f.jsx)(s,{children:c})]})}function d(e={}){let{wrapper:t}={...i(),...e.components};return t?(0,f.jsx)(t,{...e,children:(0,f.jsx)(u,{...e})}):u(e)}var f;function p(){return(p=e((()=>{f=r(),a(),n(),l(),o()})))()}p();export{d as default};
