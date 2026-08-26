a2c6336fa20e50d8e8fae7b0fa20a80d1a09b4d0
lifaon74 <lifaon74@users.noreply.github.com>
2026-08-26 12:33:05 +0000

    deploy: 7a66358ee1c1c8836ab426243eba7cfce90c0495

---
+++ storybook/mr/311/assets/text-link-BvjNBHGr.js
@@ -0,0 +1,44 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{o as t,s as n}from"./blocks-Bh_5PVZc.js";import{a as r}from"./chunk-W22LQPXL-Bced4FlR.js";import{i,r as a}from"./react-Bl2r1tuC.js";import{n as o,t as s}from"./dist-dAU-ll2Y.js";var c;function l(){return(l=e((()=>{c=`<!-- Do not edit directly, this file was auto-generated. -->
+
+<table class="token-table">
+  <thead>
+    <tr>
+      <th style="width: 20%;">Preview</th>
+      <th style="width: 80%;">Details</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td><div style=" border-radius: 4px; width: 100%; height: 75px; background: var(--esds-text-link-color); border: 1px solid #e5e7eb; " ></div> <div data-preview-value="--esds-text-link-color"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text-link.color" type="button">text-link.color</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-link-color)" type="button">var(--esds-text-link-color)</button>
+              </div><div class="token-row token-description">Default text link color.</div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" border-radius: 4px; width: 100%; height: 75px; background: var(--esds-text-link-color-visited); border: 1px solid #e5e7eb; " ></div> <div data-preview-value="--esds-text-link-color-visited"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text-link.color.visited" type="button">text-link.color.visited</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-link-color-visited)" type="button">var(--esds-text-link-color-visited)</button>
+              </div><div class="token-row token-description">Default visited text link color.</div>
+      </td>
+    </tr>
+    <tr>
+      <td><div style=" border-radius: 4px; width: 100%; height: 75px; background: var(--esds-text-link-color-focus); border: 1px solid #e5e7eb; " ></div> <div data-preview-value="--esds-text-link-color-focus"></div></td>
+      <td>
+        <div class="token-row">
+          <button class="token-value" data-clipboard="text-link.color.focus" type="button">text-link.color.focus</button>
+        </div><div class="token-row">
+                CSS: <button class="token-value" data-clipboard="var(--esds-text-link-color-focus)" type="button">var(--esds-text-link-color-focus)</button>
+              </div><div class="token-row token-description">Default focus text link color.</div>
+      </td>
+    </tr>
+  </tbody>
+</table>`})))()}function u(e){let n={h1:`h1`,...i(),...e.components};return(0,f.jsxs)(f.Fragment,{children:[(0,f.jsx)(t,{title:`Design Tokens/T3/Text Link`}),`
+`,(0,f.jsx)(n.h1,{id:`t3-text-link-tokens`,children:`T3 Text Link Tokens`}),`
+`,(0,f.jsx)(s,{children:c})]})}function d(e={}){let{wrapper:t}={...i(),...e.components};return t?(0,f.jsx)(t,{...e,children:(0,f.jsx)(u,{...e})}):u(e)}var f;function p(){return(p=e((()=>{f=r(),a(),n(),l(),o()})))()}p();export{d as default};
