f14ea47ec709b53fe9ff693e688aac1d0ad09b50
Victor Jolissaint <victor.jolissaint@infomaniak.com>
2026-09-16 15:22:05 +0200

    refactor: derive symbols Swift names from config (#359)

--- packages/assets/images/svg/scripts/shared/sf-symbols/sf-symbols-config.ts
+++ packages/assets/images/svg/scripts/shared/sf-symbols/sf-symbols-config.ts
@@ -1,7 +1,10 @@
-export const SYMBOLS_XCASSETS_DIRECTORY_NAME = 'ESDSSymbols.xcassets';
-export const SYMBOLS_SWIFT_FILE_NAME = 'ESDSSymbols.swift';
+export const SYMBOLS_ENUM_NAME = 'ESDSSymbols';
+export const SYMBOL_TYPE_NAME = 'Symbol';
+
+export const SYMBOLS_XCASSETS_DIRECTORY_NAME = `${SYMBOLS_ENUM_NAME}.xcassets`;
+export const SYMBOLS_SWIFT_FILE_NAME = `${SYMBOLS_ENUM_NAME}.swift`;
 
 export const SYMBOL_FILL_RATIO = 1;
 
-export const IOS_SYMBOLS_DESTINATION_PATH = 'Sources/ESDSSymbols/Symbols.xcassets';
-export const IOS_SYMBOLS_SWIFT_DESTINATION_PATH = `Sources/ESDSSymbols/${SYMBOLS_SWIFT_FILE_NAME}`;
+export const IOS_SYMBOLS_DESTINATION_PATH = `Sources/${SYMBOLS_ENUM_NAME}/Symbols.xcassets`;
+export const IOS_SYMBOLS_SWIFT_DESTINATION_PATH = `Sources/${SYMBOLS_ENUM_NAME}/${SYMBOLS_SWIFT_FILE_NAME}`;
