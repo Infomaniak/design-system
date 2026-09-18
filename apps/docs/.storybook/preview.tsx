c61fb81b0bdfe2defaa0cab6074a854b849e1920
lifaon74 <valentinrich@gmail.com>
2026-09-18 09:01:12 +0200

    chore(components): experiment with button type modifiers

--- apps/docs/.storybook/preview.tsx
+++ apps/docs/.storybook/preview.tsx
@@ -65,6 +65,10 @@ import '@infomaniak-design-system/tokens/dist/web/css/material/modifiers/button-
 import '@infomaniak-design-system/tokens/dist/web/css/material/modifiers/button-size/medium.attr.css';
 import '@infomaniak-design-system/tokens/dist/web/css/material/modifiers/button-size/small.attr.css';
 
+// Import all button-type modifiers
+import '@infomaniak-design-system/tokens/dist/web/css/material/modifiers/button-type/destructive.attr.css';
+import '@infomaniak-design-system/tokens/dist/web/css/material/modifiers/button-type/primary.attr.css';
+import '@infomaniak-design-system/tokens/dist/web/css/material/modifiers/button-type/secondary.attr.css';
 
 // Initialize <esds-icon> and provide IconifyApi via root InjectionContext
 EsdsIconComponent.define();
